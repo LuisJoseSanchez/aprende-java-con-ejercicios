2eecdbcff829334db352a3333653272ee7f10e72
Luis José Sánchez <luisjoseprofe@gmail.com>
2014-05-07 09:53:26 +0200

    Ejercicio 9 (Cartas sin repetir y ordenadas) más algunas correcciones

---
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio09/Carta.java
@@ -0,0 +1,33 @@
+
+public class Carta implements Comparable<Carta>{
+
+	private static String[] n = {"as", "dos", "tres", "cuatro", "cinco", "seis", "siete", "sota", "caballo", "rey"};
+	private static String[] p = {"bastos", "copas", "espadas", "oros"};
+	
+	private Integer numero;
+	private String palo;
+
+	public Carta() {
+		this.numero = (int)(Math.random()*10);
+		this.palo = p[(int)(Math.random()*4)];
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public String getPalo() {
+		return palo;
+	}
+
+	public String toString() {
+		return n[numero] + " de " + palo;
+	}
+
+	public int compareTo(Carta c) {
+		if (palo.equals(c.getPalo()))
+			return numero.compareTo(c.getNumero());
+		else
+			return palo.compareTo(c.getPalo());
+	}
+}
--- soluciones/10_Colecciones_y_diccionarios/S10Ejercicio09/Ejercicio09.java
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio09/Ejercicio09.java
@@ -1,14 +1,16 @@
 /*
- * 8. Realiza un programa que escoja al azar 10 cartas de la baraja española (10
- *    objetos de la clase Carta). Emplea un objeto de la clase ArrayList para
- *    almacenarlas y asegúrate de que no se repite ninguna.
+ * 9. Modifica el programa anterior de tal forma que las cartas se
+ *    muestren ordenadas. Primero se ordenarán por palo: bastos,
+ *    copas, espadas, oros. Cuando coincida el palo, se ordenará por
+ *    número: as, 2, 3, 4, 5, 6, 7, sota, caballo, rey.
  *    
  *    @author Luis José Sánchez
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class Ejercicio08 {
+public class Ejercicio09 {
 
 	public static void main(String[] args) {
 		
@@ -24,7 +26,9 @@ public class Ejercicio08 {
 			
 			c.add(cartaAux);
 		}
-			
+		
+		Collections.sort(c);
+		
 		for (Carta miCarta: c)  System.out.println(miCarta);
 		
 	} // main /////////////////////////////////////////////////////////
@@ -34,7 +38,7 @@ public class Ejercicio08 {
 	// de cartas
 	public static boolean existeCarta(Carta c, ArrayList<Carta> cartas) {
 		for (Carta car : cartas)
-			if ( (c.getNumero().equals(car.getNumero()))
+			if ( (c.getNumero() == car.getNumero())
 				 && (c.getPalo().equals(car.getPalo())) )
 				 return true;
 				 
