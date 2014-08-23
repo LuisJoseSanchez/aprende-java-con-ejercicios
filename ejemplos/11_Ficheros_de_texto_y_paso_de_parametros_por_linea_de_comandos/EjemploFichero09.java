b0bde01b7efb88609876fc03a042cf1763e1d6ed
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-08-23 11:58:19 +0200

    actualizaciones varias

--- ejemplos/11_Ficheros_de_texto_y_paso_de_parametros_por_linea_de_comandos/EjemploFichero09.java
+++ ejemplos/11_Ficheros_de_texto_y_paso_de_parametros_por_linea_de_comandos/EjemploFichero09.java
@@ -10,33 +10,34 @@ import java.io.*;
 
 class EjemploFichero09 {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		if (args.length != 1) {
-			System.out.println("Uso del programa: EjemploFichero09 FICHERO"); // suponemos que hemos llamado a la clase "media" en lugar de "EjemploFichero09"
-			System.exit(-1); // sale del programa
-		}
-			
-		try {		
-			BufferedReader bf = new BufferedReader(new FileReader(args[0]));
-			
-			String linea = "0";
-			int i = 0;
-			double suma = 0;
-			
-			while (linea != null) {
-				i++;
-				suma += Double.parseDouble(linea);
-				linea = bf.readLine();					
-			}
-			i--;
-			
-			bf.close();
+    if (args.length != 1) {
+      System.out.println("Este programa calcula la media de los números contenidos en un fichero.");
+      System.out.println("Uso del programa: java EjemploFichero09 FICHERO");
+      System.exit(-1); // sale del programa
+    }
+      
+    try {    
+      BufferedReader bf = new BufferedReader(new FileReader(args[0]));
+      
+      String linea = "0";
+      int i = 0;
+      double suma = 0;
+      
+      while (linea != null) {
+        i++;
+        suma += Double.parseDouble(linea);
+        linea = bf.readLine();          
+      }
+      i--;
+      
+      bf.close();
 
-			System.out.println("La media es " + suma/(double)i);
+      System.out.println("La media es " + suma/(double)i);
 
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
