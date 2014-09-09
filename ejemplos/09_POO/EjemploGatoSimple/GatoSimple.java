822cf88804a054d1c9e54bd65b39e2fe83fffbf0
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-09-09 10:53:19 +0200

    Revisando los ejemplos del capítulo 9

--- ejemplos/09_POO/EjemploGatoSimple/GatoSimple.java
+++ ejemplos/09_POO/EjemploGatoSimple/GatoSimple.java
@@ -24,14 +24,27 @@ public class GatoSimple {
     return this.sexo;
   }
   
+  /**
+   * Hace que el gato maulle
+   */
   void maulla() {
     System.out.println("Miauuuu");
   }
-  
+
+  /**
+   * Hace que el gato ronronee
+   */  
   void ronronea() {
     System.out.println("mrrrrrr");
   }
   
+  /**
+   * Hace que el gato coma.
+   * A los gatos les gusta el pescado, si le damos otra comida
+   * la rechazará.
+   * 
+   * @param comida la comida que se le ofrece al gato
+   */
   void come(String comida) {
     if (comida.equals("pescado")) {
         System.out.println("Hmmmm, gracias");
@@ -39,17 +52,22 @@ public class GatoSimple {
         System.out.println("Lo siento, yo solo como pescado");
     }
   }
-  
+
+  /**
+   * Pone a pelear dos gatos.
+   * Solo se van a pelear dos machos entre sí.
+   * 
+   * @param contrincante es el gato contra el que pelear
+   */  
   void peleaCon(GatoSimple contrincante) {
     if (this.sexo.equals("hembra")) {
-        System.out.println("no me gusta pelear");
+      System.out.println("no me gusta pelear");
     } else {
-        if (contrincante.getSexo().equals("hembra")) {
-            System.out.println("no peleo contra gatitas");
-        } else {
-            System.out.println("ven aquí que te vas a enterar");
-        }
+      if (contrincante.getSexo().equals("hembra")) {
+          System.out.println("no peleo contra gatitas");
+      } else {
+          System.out.println("ven aquí que te vas a enterar");
       }
-
+    }
   }
 }
