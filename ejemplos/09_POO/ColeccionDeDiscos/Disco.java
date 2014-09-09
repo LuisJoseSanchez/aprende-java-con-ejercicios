deff92a6009c31375bcb280816bdcaf9156c7519
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-09-09 16:18:52 +0200

    Revisados los ejemplos del capítulo 9

--- ejemplos/09_POO/ColeccionDeDiscos/Disco.java
+++ ejemplos/09_POO/ColeccionDeDiscos/Disco.java
@@ -5,59 +5,61 @@
  */
 
 public class Disco {
-	private String codigo = "LIBRE";
-	private String autor, titulo, genero;
-	private int duracion; // duración total en minutos
+  private String codigo = "LIBRE";
+  private String autor;
+  private String titulo;
+  private String genero;
+  private int duracion; // duración total en minutos
 
-	public String getCodigo() {
-		return codigo;
-	}
+  public String getCodigo() {
+    return codigo;
+  }
 
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	
-	public String getAutor() {
-		return autor;
-	}
-	
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	
-	public String getGenero() {
-		return genero;
-	}
-	
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-	
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	
-	public int getDuracion() {
-		return duracion;
-	}
-	
-	public void setDuracion(int duracion) {
-		this.duracion = duracion;
-	}
-	
-	public String toString() {
-		String cadena = "\n------------------------------------------";
-				cadena += "\nCódigo: " + this.codigo;	
-				cadena += "\nAutor: " + this.autor;		
-				cadena += "\nTítulo: " + this.titulo;
-				cadena += "\nGénero: " + this.genero;
-				cadena += "\nDuración: " + this.duracion;
-				cadena += "\n------------------------------------------";
-				
-		return cadena;
-	}
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+  
+  public String getAutor() {
+    return autor;
+  }
+  
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+  
+  public String getGenero() {
+    return genero;
+  }
+  
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+  
+  public String getTitulo() {
+    return titulo;
+  }
+  
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+  
+  public int getDuracion() {
+    return duracion;
+  }
+  
+  public void setDuracion(int duracion) {
+    this.duracion = duracion;
+  }
+  
+  public String toString() {
+    String cadena = "\n------------------------------------------";
+        cadena += "\nCódigo: " + this.codigo;  
+        cadena += "\nAutor: " + this.autor;    
+        cadena += "\nTítulo: " + this.titulo;
+        cadena += "\nGénero: " + this.genero;
+        cadena += "\nDuración: " + this.duracion;
+        cadena += "\n------------------------------------------";
+        
+    return cadena;
+  }
 }
