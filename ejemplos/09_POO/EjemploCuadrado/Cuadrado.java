aefc076aec96cb80f3255f63c0fac067204d77e5
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-09-09 11:06:03 +0200

    Revisando los ejemplos del capítulo 9

--- ejemplos/09_POO/EjemploCuadrado/Cuadrado.java
+++ ejemplos/09_POO/EjemploCuadrado/Cuadrado.java
@@ -5,31 +5,36 @@
  */
 
 public class Cuadrado {
-	
-	int lado;
+  
+  int lado;
     
-	Cuadrado (int l) {
-		this.lado = l;
-	}
-	
-	public String toString() {
-        int i, espacios;
-        String resultado = "";
+  public Cuadrado(int l) {
+    this.lado = l;
+  }
+  
+  public String toString() {
+    
+    int i, espacios;
+    String resultado = "";
 
-		for (i = 0; i < this.lado; i++)  resultado += "██";
-        resultado += "\n";
+    for (i = 0; i < this.lado; i++) {
+      resultado += "██";
+    }
+    resultado += "\n";
 
-		for (i = 1; i < this.lado - 1; i++) {
-            resultado += "██";
-            for (espacios = 1; espacios < this.lado - 1; espacios++)
-                resultado += "  ";
-            resultado += "██\n";
-        }
+    for (i = 1; i < this.lado - 1; i++) {
+      resultado += "██";
+      for (espacios = 1; espacios < this.lado - 1; espacios++) {
+        resultado += "  ";
+      }
+      resultado += "██\n";
+    }
         
-        for (i = 0; i < this.lado; i++)  resultado += "██";
-        resultado += "\n";
-        
-        return resultado;
-	}
-	
+    for (i = 0; i < this.lado; i++) {
+      resultado += "██";
+    }
+    resultado += "\n";
+    
+    return resultado;
+  }
 }
--- ejemplos/09_POO/EjemploCuadrado/PruebaCuadrado.java
+++ ejemplos/09_POO/EjemploCuadrado/PruebaCuadrado.java
@@ -5,11 +5,9 @@
  */
 
 public class PruebaCuadrado {
-
-	public static void main(String[] args) {
-		
-		Cuadrado miCuadradito = new Cuadrado(5);
-		
-        System.out.println(miCuadradito);
-	}
+  public static void main(String[] args) {
+    
+    Cuadrado miCuadradito = new Cuadrado(5);
+    System.out.println(miCuadradito);
+  }
 }
