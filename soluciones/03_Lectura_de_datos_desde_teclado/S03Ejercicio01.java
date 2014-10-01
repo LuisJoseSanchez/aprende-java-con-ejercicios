b47ee255ae41f419c7dbb9ebfd6d91dc46092d30
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-01 12:54:15 +0200

    Revisando soluciones del capítulo 3

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio01.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio01.java
@@ -1,26 +1,25 @@
 /**
- * 
  * 3. Lectura de datos desde teclado
  *
  * 1. Realiza un programa que pida dos números y que luego muestre el
  *    resultado de su multiplicación.
  *
  * @author Luis José Sánchez
- * 
  */
 
 public class S03Ejercicio01 {
-	public static void main(String[] args) {
-		int x, y;
-		String linea;
+  public static void main(String[] args) {
+    int x;
+    int y;
+    String linea;
 
-		System.out.print("Por favor, introduce el primer número: ");
-		linea = System.console().readLine();
-		x = Integer.parseInt( linea );
-		System.out.print("Introduce el segundo número: ");
-		linea = System.console().readLine();
-		y = Integer.parseInt( linea );
-		
-		System.out.println(x + "*" + y + "=" + (x * y));
-	}
+    System.out.print("Por favor, introduce el primer número: ");
+    linea = System.console().readLine();
+    x = Integer.parseInt( linea );
+    System.out.print("Introduce el segundo número: ");
+    linea = System.console().readLine();
+    y = Integer.parseInt( linea );
+    
+    System.out.println(x + "*" + y + "=" + (x * y));
+  }
 }
