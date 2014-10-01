74f0cf423c68d503da7e220015270e74eec39e35
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-01 12:57:52 +0200

    Revisando soluciones del capítulo 3

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio06.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio06.java
@@ -1,21 +1,19 @@
 /**
- * 
  * 3. Lectura de datos desde teclado
  *
  * 6. Escribe un programa que calcule el área de un triángulo.
  *
  * @author Luis José Sánchez
- * 
  */
 
 public class S03Ejercicio06 {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		System.out.println("Área de un triángulo");
-		System.out.print("Por favor, introduzca la longitud de la base (cm): ");
-		double base = Double.parseDouble(System.console().readLine());
-		System.out.print("Inroduzca la altura (cm): ");
-		double altura = Double.parseDouble(System.console().readLine());
-		System.out.println("El área del triángulo es " + (base * altura)/2 + " cm2");
-	}
+    System.out.println("Área de un triángulo");
+    System.out.print("Por favor, introduzca la longitud de la base (cm): ");
+    double base = Double.parseDouble(System.console().readLine());
+    System.out.print("Inroduzca la altura (cm): ");
+    double altura = Double.parseDouble(System.console().readLine());
+    System.out.println("El área del triángulo es " + (base * altura)/2 + " cm2");
+  }
 }
