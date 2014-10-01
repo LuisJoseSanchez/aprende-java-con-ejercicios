86b9775a2b854bdb1a901e23d6c748b71471c1fd
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-01 13:03:19 +0200

    Revisadas las soluciones del capítulo 3

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio09.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio09.java
@@ -1,29 +1,27 @@
 /**
- * 
  * 3. Lectura de datos desde teclado
  *
  * 9. Escribe un programa que calcule el volumen de un cono según la
  *    fórmula V = (1/3)πr^2h
  *
  * @author Luis José Sánchez
- * 
  */
 
 public class S03Ejercicio09 {
-	
-	// Las constantes se deben declarar antes del main
-	static final double pi = 3.141592654;
-	// En lugar de definir una constante se podría usar
-	// la constante predefinida Math.PI 
-	
-	public static void main(String[] args) {
+  
+  // Las constantes se deben declarar antes del main
+  static final double pi = 3.141592654;
+  // En lugar de definir una constante se podría usar
+  // la constante predefinida Math.PI 
+  
+  public static void main(String[] args) {
 
-		System.out.println("Volumen de un cono");
-		System.out.print("Por favor, introduzca la altura (cm): ");
-		double h = Double.parseDouble(System.console().readLine());
-		System.out.print("Introduzca el radio (cm): ");
-		double r = Double.parseDouble(System.console().readLine());
-		double v = (1.0/3.0) * pi * r * r * h;
-		System.out.println("El volumen del cono es de " +  v + " cm3");
-	}
+    System.out.println("Volumen de un cono");
+    System.out.print("Por favor, introduzca la altura (cm): ");
+    double h = Double.parseDouble(System.console().readLine());
+    System.out.print("Introduzca el radio (cm): ");
+    double r = Double.parseDouble(System.console().readLine());
+    double v = (1.0/3.0) * pi * r * r * h;
+    System.out.println("El volumen del cono es de " +  v + " cm3");
+  }
 }
