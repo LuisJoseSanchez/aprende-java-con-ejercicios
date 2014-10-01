2e645f9608fbae9106364b68115a977c92bb6a2e
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-01 12:55:37 +0200

    Revisando soluciones del capítulo 3

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio02.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio02.java
@@ -1,22 +1,20 @@
 /**
- * 
  * 3. Lectura de datos desde teclado
  * 
  * 2. Realiza un conversor de euros a pesetas. La cantidad de euros que
  *    se quiere convertir debe ser introducida por teclado.
  *
  * @author Luis José Sánchez
- * 
  */
 
 public class S03Ejercicio02 {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		System.out.print("Por favor, introduce la cantidad de euros que quieres convertir: ");
-		double euros = Double.parseDouble(System.console().readLine());
+    System.out.print("Por favor, introduce la cantidad de euros que quieres convertir: ");
+    double euros = Double.parseDouble(System.console().readLine());
 
-		int pesetas = (int) (euros * 166.386);
-		
-		System.out.print(euros + " euros son " + pesetas + " pesetas.");
-	}
+    int pesetas = (int) (euros * 166.386);
+    
+    System.out.print(euros + " euros son " + pesetas + " pesetas.");
+  }
 }
