f7acec1b0012bcccfdfa8cdc95724f04d3488dde
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-22 12:11:43 +0200

    Revisando soluciones del capítulo 4

--- soluciones/04_Sentencia_condicional/S04Ejercicio17.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio17.java
@@ -6,13 +6,12 @@
  *
  * @author Luis José Sánchez
  */
-
 public class S04Ejercicio17 {
 
-	public static void main(String[] args) {
-		
-		System.out.print("Por favor, introduzca un número entero: ");
-		int n = Integer.parseInt(System.console().readLine());
-		System.out.println("La última cifra del número introducido es el " + (n % 10));
-	}
+  public static void main(String[] args) {
+    
+    System.out.print("Por favor, introduzca un número entero: ");
+    int n = Integer.parseInt(System.console().readLine());
+    System.out.println("La última cifra del número introducido es el " + (n % 10));
+  }
 }
