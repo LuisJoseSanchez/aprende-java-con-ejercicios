e5673fe8944886cb3d4a2b1dde410335cad7aa06
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-22 11:57:40 +0200

    Revisando soluciones del capítulo 4

--- soluciones/04_Sentencia_condicional/S04Ejercicio11.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio11.java
@@ -6,24 +6,23 @@
  *
  * @author Luis José Sánchez
  */
-
 public class S04Ejercicio11 {
 
-	public static void main(String[] args) {
-		
-		System.out.print("A continuación deberá introducir una hora del día, ");
-		System.out.println("primero introducirá la hora y luego los minutos.");
-		
-		System.out.print("hora: ");
-		int hora = Integer.parseInt(System.console().readLine());	
-		
-		System.out.print("minuto: ");
-		int minuto = Integer.parseInt(System.console().readLine());
+  public static void main(String[] args) {
+    
+    System.out.print("A continuación deberá introducir una hora del día, ");
+    System.out.println("primero introducirá la hora y luego los minutos.");
+    
+    System.out.print("hora: ");
+    int hora = Integer.parseInt(System.console().readLine());  
+    
+    System.out.print("minuto: ");
+    int minuto = Integer.parseInt(System.console().readLine());
 
-		int segundosTranscurridos = (hora * 3600) + (minuto * 60);
-		int segundosHastaMedianoche = (24 * 3600) - segundosTranscurridos;
-						
-		//System.out.println("Desde las " + hora + ":" + minuto + " hasta la medianoche faltan " + segundosHastaMedianoche + " segundos.");
-		System.out.printf("Desde las %02d:%02d hasta la medianoche faltan %d segundos", hora, minuto, segundosHastaMedianoche);
-	}
+    int segundosTranscurridos = (hora * 3600) + (minuto * 60);
+    int segundosHastaMedianoche = (24 * 3600) - segundosTranscurridos;
+            
+    //System.out.println("Desde las " + hora + ":" + minuto + " hasta la medianoche faltan " + segundosHastaMedianoche + " segundos.");
+    System.out.printf("Desde las %02d:%02d hasta la medianoche faltan %d segundos", hora, minuto, segundosHastaMedianoche);
+  }
 }
