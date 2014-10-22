4c3de24875a69e4a685d31af4a4bb1d2ffac4a40
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-22 12:23:30 +0200

    Revisandas las soluciones del capítulo 4

--- soluciones/04_Sentencia_condicional/S04Ejercicio20.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio20.java
@@ -19,39 +19,39 @@ public class S04Ejercicio20 {
     
     
     // número de una cifra
-    if ( n < 10 ) {
+    if (n < 10) {
       capicua = true;
     }
     
     // número de dos cifras
-    if (( n >= 10 ) && ( n < 100 )) {
-      if ( (n / 10) == (n % 10) ) {
+    if ((n >= 10) && (n < 100)) {
+      if ((n / 10) == (n % 10)) {
         capicua = true;
       }
     }
 
     // número de tres cifras
-    if (( n >= 100 ) && ( n < 1000 )) {
-      if ( (n / 100) == (n % 10) ) {
+    if ((n >= 100) && (n < 1000)) {
+      if ((n / 100) == (n % 10)) {
         capicua = true;
       }
     }
 
     // número de cuatro cifras
-    if (( n >= 1000 ) && ( n < 10000 )) {
-      if ((( n / 1000 ) == (n % 10) ) && ( (( n / 100 ) % 10 )== (( n / 10) % 10 ))) {
+    if ((n >= 1000) && (n < 10000)) {
+      if (((n / 1000) == (n % 10)) && ((( n / 100 ) % 10)== (( n / 10) % 10))) {
         capicua = true;
       }
     }
     
     // número de cinco cifras
-    if ( n >= 10000) {
-      if ((( n / 10000 ) == (n % 10) ) && ( ( ((n / 1000) % 10) ) == (( n / 10) % 10))) {
+    if (n >= 10000) {
+      if (((n / 10000) == (n % 10) ) && ((((n / 1000) % 10)) == ((n / 10) % 10))) {
         capicua = true;
       }
     }
     
-    if ( capicua ) {
+    if (capicua) {
       System.out.println("El número introducido es capicúa.");
     } else {
       System.out.println("El número introducido no es capicúa.");
