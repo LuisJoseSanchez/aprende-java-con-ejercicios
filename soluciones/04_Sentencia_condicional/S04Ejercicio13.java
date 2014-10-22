f4a54e9a9cdfb8809c19cc2ff429aac4fdc82520
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-22 12:02:09 +0200

    Revisando soluciones del capítulo 4

--- soluciones/04_Sentencia_condicional/S04Ejercicio13.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio13.java
@@ -5,40 +5,39 @@
  *
  * @author Luis José Sánchez
  */
-
 public class S04Ejercicio13 {
 
-	public static void main(String[] args) {
-		
-		int aux;
-				
-		System.out.println("Este programa ordena tres números introducidos por teclado.");
-		System.out.println("Por favor, vaya introduciendo los tres números y pulsando INTRO:");
-		int a = Integer.parseInt(System.console().readLine());
-		int b = Integer.parseInt(System.console().readLine());
-		int c = Integer.parseInt(System.console().readLine());
+  public static void main(String[] args) {
+    
+    int aux;
+        
+    System.out.println("Este programa ordena tres números introducidos por teclado.");
+    System.out.println("Por favor, vaya introduciendo los tres números y pulsando INTRO:");
+    int a = Integer.parseInt(System.console().readLine());
+    int b = Integer.parseInt(System.console().readLine());
+    int c = Integer.parseInt(System.console().readLine());
 
-		// ordenación de los dos primeros números
-		if (a > b) {
-			aux = a;
-			a = b;
-			b = aux;
-		}
-		
-		// ordenación de los dos últimos números
-		if (b > c) {
-			aux = b;
-			b = c;
-			c = aux;
-		}
-		
-		// se vuelven a ordenar los dos primeros
-		if (a > b) {
-			aux = a;
-			a = b;
-			b = aux;
-		}
-				
-		System.out.println("Los números introducidos ordenados de menor a mayor son " + a + ", " + b + " y " + c + "."); 
-	}
+    // ordenación de los dos primeros números
+    if (a > b) {
+      aux = a;
+      a = b;
+      b = aux;
+    }
+    
+    // ordenación de los dos últimos números
+    if (b > c) {
+      aux = b;
+      b = c;
+      c = aux;
+    }
+    
+    // se vuelven a ordenar los dos primeros
+    if (a > b) {
+      aux = a;
+      a = b;
+      b = aux;
+    }
+        
+    System.out.println("Los números introducidos ordenados de menor a mayor son " + a + ", " + b + " y " + c + "."); 
+  }
 }
