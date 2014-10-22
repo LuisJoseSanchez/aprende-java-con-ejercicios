f09c3af742457ec0074cd9ddc7c218babf579014
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-22 12:32:58 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio07.java
+++ soluciones/05_Bucles/S05Ejercicio07.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 5. Bucles
  * 
  * 7. Realiza el control de acceso a una caja fuerte. La combinación
@@ -10,35 +9,33 @@
  *    oportunidades para abrir la caja fuerte.
  * 
  * @author Luis José Sánchez
- * 
  */
-
 public class S05Ejercicio07 {
 
-	public static void main(String[] args) {
-		
-        int intentos = 4, numeroIntroducido;
-        boolean acertado = false;
-        
-        do {
-            
-            System.out.print("Introduzca la clave de la caja fuerte: ");
-            numeroIntroducido = Integer.parseInt(System.console().readLine());
-            
-            if (numeroIntroducido == 8888)
-                    acertado = true;
-                else
-                    System.out.println("Clave incorrecta");
-            
-            intentos--;
-            
-         } while((intentos > 0) && (!acertado));
-        
-        
-        if (acertado)
-                System.out.println("Ha abierto la caja fuerte.");
-            else
-                System.out.println("Lo siento, ha agotado las 4 oportunidades.");
-
-	}
+  public static void main(String[] args) {
+    
+    int intentos = 4;
+    int numeroIntroducido;
+    boolean acertado = false;
+    
+    do {
+      System.out.print("Introduzca la clave de la caja fuerte: ");
+      numeroIntroducido = Integer.parseInt(System.console().readLine());
+      
+      if (numeroIntroducido == 8888) {
+        acertado = true;
+      } else {
+        System.out.println("Clave incorrecta");
+      }
+      
+      intentos--;
+  
+    } while((intentos > 0) && (!acertado));
+    
+    if (acertado) {
+      System.out.println("Ha abierto la caja fuerte.");
+    } else {
+      System.out.println("Lo siento, ha agotado las 4 oportunidades.");
+    }
+  }
 }
