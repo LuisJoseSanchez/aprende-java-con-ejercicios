f5b9cf193380e960bfa14adcb0e3530e7a079f35
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 09:04:52 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio13.java
+++ soluciones/05_Bucles/S05Ejercicio13.java
@@ -1,29 +1,30 @@
 /**
- * 
  * 5. Bucles
  * 
  * 13. Escribe un programa que lea una lista de diez números y determine
  *     cuántos son positivos, y cuántos son negativos.
  * 
  * @author Luis José Sánchez
- * 
  */
 
 public class S05Ejercicio13 {
 
-	public static void main(String[] args) {
-		
-        int negativos = 0, positivos = 0;
-        
-        System.out.println("Por favor, introduzca 10 números enteros: ");
+  public static void main(String[] args) {
+  
+    System.out.println("Por favor, introduzca 10 números enteros: ");
 
-        for (int i = 0; i < 10; i++)
-            if (Integer.parseInt(System.console().readLine()) < 0)
-                    negativos++;
-                else
-                    positivos++;
+    int negativos = 0;
+    int positivos = 0;
+    
+    for (int i = 0; i < 10; i++) {
+      if (Integer.parseInt(System.console().readLine()) < 0) {
+        negativos++;
+      } else {
+        positivos++;
+      }
+    }
+    
+    System.out.println("Ha introducido " + positivos + " positivos y " + negativos + " negativos");
 
-        System.out.println("Ha introducido " + positivos + " positivos y " + negativos + " negativos");
-
-	}
+  }
 }
