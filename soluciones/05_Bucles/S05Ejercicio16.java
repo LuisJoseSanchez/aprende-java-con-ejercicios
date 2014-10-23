d5b7bd42af8034c17a7f898b739d22fb182dbcb8
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 09:32:49 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio16.java
+++ soluciones/05_Bucles/S05Ejercicio16.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 5. Bucles
  * 
  * 16. Escribe un programa que diga si un número introducido por teclado
@@ -7,25 +6,28 @@
  *     él mismo y la unidad.
  * 
  * @author Luis José Sánchez
- * 
  */
 
 public class S05Ejercicio16 {
 
-	public static void main(String[] args) {
-		
-        boolean esPrimo = true;
-                
-        System.out.print("Introduzca un número entero y le diré si es primo: ");
-        int numeroIntroducido = Integer.parseInt(System.console().readLine());
-    
-        for (int i = 2; i < numeroIntroducido; i++)
-            if (numeroIntroducido%i == 0) esPrimo = false;
+  public static void main(String[] args) {
             
-        if (esPrimo)
-                System.out.println("El número introducido es primo.");
-            else
-                System.out.println("El número introducido no es primo.");
+    System.out.print("Introduzca un número entero y le diré si es primo: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
 
-	}
+    boolean esPrimo = true;
+    
+    for (int i = 2; i < numeroIntroducido; i++) {
+      if ((numeroIntroducido % i) == 0) {
+        esPrimo = false;
+      }
+    }
+        
+    if (esPrimo) {
+      System.out.println("El número introducido es primo.");
+    } else {
+      System.out.println("El número introducido no es primo.");
+    }
+
+  }
 }
