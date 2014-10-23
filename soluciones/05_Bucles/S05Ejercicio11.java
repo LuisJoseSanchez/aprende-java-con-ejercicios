b017cae66e04d065eb338b13482de8bce2a4daec
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 08:58:16 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio11.java
+++ soluciones/05_Bucles/S05Ejercicio11.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 5. Bucles
  * 
  * 11. Escribe un programa que muestre en tres columnas, el cuadrado y
@@ -7,18 +6,17 @@
  *     introduce por teclado.
  * 
  * @author Luis José Sánchez
- * 
  */
 
 public class S05Ejercicio11 {
 
-	public static void main(String[] args) {
-		
-        System.out.print("Introduzca un número: ");
-        int numeroIntroducido = Integer.parseInt(System.console().readLine());
+  public static void main(String[] args) {
+    
+    System.out.print("Introduzca un número: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
 
-        
-        for (int i = numeroIntroducido; i < numeroIntroducido + 5; i++)
-            System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
-	}
+    for (int i = numeroIntroducido; i < numeroIntroducido + 5; i++) {
+      System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
+    }
+  }
 }
