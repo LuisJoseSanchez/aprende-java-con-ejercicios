1ac216cdb643a20378b7561fbbdf7df85ec34477
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 10:09:44 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio25.java
+++ soluciones/05_Bucles/S05Ejercicio25.java
@@ -1,29 +1,26 @@
 /**
- * 
  * 5. Bucles
  * 
  * 25. Realiza un programa que pida un número por teclado y que luego
  *     muestre ese número al revés.
  * 
  * @author Luis José Sánchez
- * 
  */
-
 public class S05Ejercicio25 {
 
-	public static void main(String[] args) {
-		
-		int volteado = 0, numero;
-		
-        System.out.print("Introduzca un número entero: ");
-        int numeroIntroducido = Integer.parseInt(System.console().readLine());
+  public static void main(String[] args) {
 
-        numero = numeroIntroducido;
-		while (numero > 0) {
-			volteado = (volteado * 10) + (numero % 10);
-			numero /= 10;
-		} // while
-		
-		System.out.println("Si le damos la vuelta al " + numeroIntroducido + " tenemos el " + volteado);
-	}
+    System.out.print("Introduzca un número entero: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+
+    int numero = numeroIntroducido;
+    int volteado = 0;
+    
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+    } // while
+    
+    System.out.println("Si le damos la vuelta al " + numeroIntroducido + " tenemos el " + volteado);
+  }
 }
