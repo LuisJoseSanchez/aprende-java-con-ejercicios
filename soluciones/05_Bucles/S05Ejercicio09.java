d44ad398eab8927d13aeb6dca55fcacdcdf7b154
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 08:56:09 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio09.java
+++ soluciones/05_Bucles/S05Ejercicio09.java
@@ -1,30 +1,28 @@
 /**
- * 
  * 5. Bucles
  * 
  * 9. Realiza un programa que nos diga cuántos dígitos tiene un número introducido por teclado.
  * 
  * @author Luis José Sánchez
- * 
  */
 
 public class S05Ejercicio09 {
 
-	public static void main(String[] args) {
-		
-        int numeroDeDigitos = 1, n, numeroIntroducido;
+  public static void main(String[] args) {
+    
+    int numeroDeDigitos = 1, n, numeroIntroducido;
 
-        System.out.print("Introduzca un número entero y le diré cuántos dígitos tiene: ");
-        numeroIntroducido = Integer.parseInt(System.console().readLine());
-        
-        n = numeroIntroducido;
-        
-        while (n > 10) {
-            n /= 10;
-            numeroDeDigitos++;
-        }
-        
-        System.out.println(numeroIntroducido + " tiene " + numeroDeDigitos + " dígito/s.");
+    System.out.print("Introduzca un número entero y le diré cuántos dígitos tiene: ");
+    numeroIntroducido = Integer.parseInt(System.console().readLine());
+    
+    n = numeroIntroducido;
+    
+    while (n > 10) {
+      n /= 10;
+      numeroDeDigitos++;
+    }
+    
+    System.out.println(numeroIntroducido + " tiene " + numeroDeDigitos + " dígito/s.");
 
-	}
+  }
 }
