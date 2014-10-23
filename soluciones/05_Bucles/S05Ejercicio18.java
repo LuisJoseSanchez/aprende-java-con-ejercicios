0a4db63686328edf8de5bc1d0b6bc3bd0a07e793
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 09:37:57 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio18.java
+++ soluciones/05_Bucles/S05Ejercicio18.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 5. Bucles
  * 
  * 18. Escribe un programa que obtenga los números enteros comprendidos entre dos números
@@ -7,34 +6,39 @@
  *     el menor de los enteros introducidos e ir incrementando de 7 en 7.
  * 
  * @author Luis José Sánchez
- * 
  */
 
 public class S05Ejercicio18 {
 
-	public static void main(String[] args) {
-		
-		int primerNumero, segundoNumero;
-		
-		do {
-			System.out.print("Introduzca un número entero: ");
-			primerNumero = Integer.parseInt(System.console().readLine());
-			System.out.print("Introduzca otro número entero distinto al anterior: ");
-			segundoNumero = Integer.parseInt(System.console().readLine());
-			if(primerNumero == segundoNumero)
-				System.out.println("Los números introducidos no son válidos, deben ser distintos.");
-		} while (primerNumero == segundoNumero);
-		
-		if (primerNumero > segundoNumero) {
-			int aux = primerNumero;
-			primerNumero = segundoNumero;
-			segundoNumero = aux;
-		}
-		
-		for(int i = primerNumero; i <= segundoNumero; i+=7)
-			System.out.print(i + " ");
-		
-		System.out.println();
+  public static void main(String[] args) {
+    
+    int primerNumero;
+    int segundoNumero;
+    
+    // pide dos números y se verifica que sean distintos
+    do {
+      System.out.print("Introduzca un número entero: ");
+      primerNumero = Integer.parseInt(System.console().readLine());
+      System.out.print("Introduzca otro número entero distinto al anterior: ");
+      segundoNumero = Integer.parseInt(System.console().readLine());
+      
+      if(primerNumero == segundoNumero) {
+        System.out.println("Los números introducidos no son válidos, deben ser distintos.");
+      }
+    } while (primerNumero == segundoNumero);
+    
+    // si el primer número es mayor que el segundo, se intercambian los valores
+    if (primerNumero > segundoNumero) {
+      int aux = primerNumero;
+      primerNumero = segundoNumero;
+      segundoNumero = aux;
+    }
+    
+    for(int i = primerNumero; i <= segundoNumero; i+=7) {
+      System.out.print(i + " ");
+    }
+    
+    System.out.println();
 
-	}
+  }
 }
