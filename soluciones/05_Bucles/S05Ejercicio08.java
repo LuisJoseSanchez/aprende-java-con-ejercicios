847b3c42feb70221646c610076a312ef4c79401b
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-10-23 08:55:03 +0200

    Revisando soluciones del capítulo 5

--- soluciones/05_Bucles/S05Ejercicio08.java
+++ soluciones/05_Bucles/S05Ejercicio08.java
@@ -1,22 +1,20 @@
 /**
- * 
  * 5. Bucles
  * 
  * 8. Muestra la tabla de multiplicar de un número introducido por teclado.
  * 
  * @author Luis José Sánchez
- * 
  */
-
 public class S05Ejercicio08 {
 
-	public static void main(String[] args) {
-		
-        System.out.print("Introduzca un número entero y le mostraré la tabla de multiplicar: ");
-        int numeroIntroducido = Integer.parseInt(System.console().readLine());
-            
-        for (int i = 0; i <= 10; i++)
-            System.out.println(numeroIntroducido + " x " + i + " = " + numeroIntroducido * i);
+  public static void main(String[] args) {
+    
+    System.out.print("Introduzca un número entero y le mostraré la tabla de multiplicar: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+        
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(numeroIntroducido + " x " + i + " = " + numeroIntroducido * i);
+    }
 
-	}
+  }
 }
