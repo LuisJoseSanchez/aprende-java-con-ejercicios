894683771fd939b987712b6cefbe251aa7216051
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-17 13:05:00 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio05.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio05.java
@@ -11,14 +11,23 @@ public class S06Ejercicio05 {
 
   public static void main(String[] args) {
     
-    int maximo = 100, minimo = 199, suma = 0, n;
+    int maximo = 100;
+    int minimo = 199;
+    int suma = 0;
+    int n;
     
     for (int i = 0; i < 50; i++) {
       n = (int)(Math.random()*100) + 100;
       System.out.print(n + " ");
       suma += n;
-      if (n < minimo) minimo = n;
-      if (n > maximo) maximo = n;
+      
+      if (n < minimo) {
+        minimo = n;
+      }
+      
+      if (n > maximo) {
+        maximo = n;
+      }
     }
     
     System.out.println("\nMínimo: " + minimo + "\nMáximo: " + maximo + "\nMedia: " + suma/50);
