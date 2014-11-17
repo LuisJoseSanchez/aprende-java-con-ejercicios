aa616f21e38beb760c69fcac0c7f6f4fb36b0c2c
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-17 13:09:37 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio06.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio06.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 6. Números aleatorios
  * 
  * 6. Escribe un programa que piense un número al azar entre 0 y 100. El usuario debe *    adivinarlo y tiene para ello 5 oportunidades. Después de cada intento fallido, el *    programa dirá cuántas oportunidades quedan y si el número introducido es menor o  *    mayor que el que ha pensado.
@@ -11,14 +10,14 @@ public class S06Ejercicio06 {
 
   public static void main(String[] args) {
     
-    int oportunidades = 5, numeroIntroducido;
+    int oportunidades = 5;
+    int numeroIntroducido;
     boolean acertado = false;
-    
     int numeroMisterioso = (int)(Math.random()*101);
     
     System.out.println("Estoy pensando un número del 0 al 100, intenta adivinarlo. Tienes 5 oportunidades.");
 
-    while(!acertado && (oportunidades > 0)) {
+    do {
       System.out.print("Introduce un número: ");
       numeroIntroducido = Integer.parseInt(System.console().readLine());
       oportunidades--;
@@ -37,12 +36,10 @@ public class S06Ejercicio06 {
         acertado = true;
         System.out.println("¡Enhorabuena! ¡has acertado!");
       }
-        
-    }
+    } while (!acertado && (oportunidades > 0));
     
-    if (!acertado)
+    if (!acertado) {
       System.out.println("Lo siento, no has acertado, el número que estaba pensando era el " + numeroMisterioso);
-
-    
+    }
   }
 }
