7c68e82564820744dd554fd9badf3ccc62f8d211
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 08:51:03 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio08.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio08.java
@@ -11,11 +11,16 @@ public class S06Ejercicio08 {
   public static void main(String[] args) {
 
     int resultadoPartido;
+    int columnas = 3;
     
-    for (int fila = 1; fila <= 14; fila++) {
+    for (int fila = 1; fila <= 15; fila++) {
       System.out.printf("%4d. |", fila);
+
+      if (fila == 15) {
+        columnas = 1;
+      }
       
-      for (int apuesta = 1; apuesta <= 3; apuesta++) {
+      for (int apuesta = 1; apuesta <= columnas; apuesta++) {
         resultadoPartido = (int)(Math.random() * 6) + 1;
         switch(resultadoPartido) {
           case 1:
@@ -33,22 +38,6 @@ public class S06Ejercicio08 {
         }
       }
       System.out.println();
-    }
-    System.out.print("\nPleno al quince: ");
-    resultadoPartido = (int)(Math.random() * 6) + 1;
-    switch(resultadoPartido) {
-          case 1:
-          case 2:
-          case 3:
-            System.out.print("1");
-            break;
-          case 4:
-          case 5:
-            System.out.print("X");
-            break;
-          case 6:
-            System.out.print("2");
-          default:
     }
   }
 }
