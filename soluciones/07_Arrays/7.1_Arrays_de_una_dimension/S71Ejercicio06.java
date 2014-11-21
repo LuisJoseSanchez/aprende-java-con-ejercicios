05be61de62666caf3a1de2e2f1cbbc4fcc80e311
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 10:24:19 +0100

    7.4.1 Arrays de una dimensión. Revisando soluciones.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio06.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio06.java
@@ -5,47 +5,62 @@
  *    elementos de ese array, es decir, el elemento de la posición 0 debe pasar a la posición 1,
  *    el de la 1 a la 2, etc. El número que se encuentra en la última posición debe pasar a la
  *    posición 0. Finalmente, muestra el contenido del array.
- * 
- *    Nota: Ajusta el tamaño de letra y las dimensiones del terminal para ver el resultado correctamente.
  *
  * @author Luis José Sánchez
  */
-
 public class S71Ejercicio06 {
 
-	public static void main(String[] args) {
-		
-			int[] numero = new int[15];
-			int i;
-			
-			System.out.println("Vaya introduciendo números enteros y pulsando INTRO:");
-			
-			for (i = 0; i < 15; i++)
-				numero[i] = Integer.parseInt(System.console().readLine());
-			
-			System.out.println();
-			
-			System.out.println("Array original:");
-			for (i = 0; i < 15; i++) System.out.printf("|%4d ", i);
-			System.out.println("|");
-			for (i = 0; i < 90; i++) System.out.print("-");
-			System.out.println("-");
-			for (i = 0; i < 15; i++) System.out.printf("|%4d ", numero[i]);
-			System.out.println("|");
+  public static void main(String[] args) {
+    
+      int[] numero = new int[15];
+      int i;
+      
+      System.out.println("Vaya introduciendo números enteros y pulsando INTRO:");
+      
+      for (i = 0; i < 15; i++) {
+        numero[i] = Integer.parseInt(System.console().readLine());
+      }
+      
+      System.out.println();
+      
+      // Muestra el array original ///////////////
+      System.out.println("Array original:");
+      for (i = 0; i < 15; i++) {
+        System.out.printf("|%3d ", i);
+      }
+      System.out.println("|");
+      for (i = 0; i < 75; i++) {
+        System.out.print("⎯");
+      }
+      System.out.println("⎯");
+      for (i = 0; i < 15; i++) {
+        System.out.printf("|%3d ", numero[i]);
+      }
+      System.out.println("|");
+      ////////////////////////////////////////////
+      
+      // rota una posición a la derecha //////////
+      int aux = numero[14];
+      for (i = 14; i > 0; i--) {
+        numero[i] = numero[i-1];
+      }
+      numero[0] = aux;
+      ////////////////////////////////////////////
 
-			// rota una posición a la derecha ///////
-			int aux = numero[14];
-			for (i = 14; i > 0; i--)
-				numero[i] = numero[i-1];
-			numero[0] = aux;
-			////////////////////////////////////////
-
-			System.out.println("\nArray rotado a la derecha una posición:");
-			for (i = 0; i < 15; i++) System.out.printf("|%4d ", i);
-			System.out.println("|");
-			for (i = 0; i < 90; i++) System.out.print("-");
-			System.out.println("-");
-			for (i = 0; i < 15; i++) System.out.printf("|%4d ", numero[i]);
-			System.out.println("|");
-	}
+      // Muestra el array rotado /////////////////
+      System.out.println("\nArray rotado a la derecha una posición:");
+      for (i = 0; i < 15; i++) {
+        System.out.printf("|%3d ", i);
+      }
+      System.out.println("|");
+      for (i = 0; i < 75; i++) {
+        System.out.print("⎯");
+      }
+      System.out.println("⎯");
+      for (i = 0; i < 15; i++) {
+        System.out.printf("|%3d ", numero[i]);
+      }
+      System.out.println("|");
+      ////////////////////////////////////////////
+  }
 }
