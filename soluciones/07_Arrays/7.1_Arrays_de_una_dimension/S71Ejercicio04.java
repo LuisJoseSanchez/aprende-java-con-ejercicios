ea3fdc3e0b756263af9ab5431b2a197193f86412
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:49:02 +0100

    7.4.1 Arrays de una dimensión. Revisando soluciones.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio04.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio04.java
@@ -14,24 +14,25 @@
 
 public class S71Ejercicio04 {
 
-	public static void main(String[] args) {
-		
-			int[] numero = new int[20];
-			int[] cuadrado = new int[20];
-			int[] cubo = new int[20];
+  public static void main(String[] args) {
+    
+      int[] numero = new int[20];
+      int[] cuadrado = new int[20];
+      int[] cubo = new int[20];
 
-			int i;
-			
-			for (i = 0; i < 20; i++) {
-				numero[i] = (int)(Math.random()*101);
-				cuadrado[i] = numero[i] * numero[i];
-				cubo[i] = cuadrado[i] * numero[i];
-			}
-			
-			System.out.println("\nA continuación se muestran en tres columnas, un número aleatorio entre 0 y 100, su cuadrado y su cubo:\n");
-			
-			System.out.println("  n  │   n²  │    n³\n─────┼───────┼─────────");
-			for (i = 0; i < 20; i++)
-				System.out.printf("%4d │ %5d │%8d\n", numero[i], cuadrado[i], cubo[i]);
-	}
+      int i;
+      
+      for (i = 0; i < 20; i++) {
+        numero[i] = (int)(Math.random()*101);
+        cuadrado[i] = numero[i] * numero[i];
+        cubo[i] = cuadrado[i] * numero[i];
+      }
+      
+      System.out.println("\nA continuación se muestran en tres columnas, un número aleatorio entre 0 y 100, su cuadrado y su cubo:\n");
+      
+      System.out.println("  n  │   n²  │    n³\n─────┼───────┼─────────");
+      for (i = 0; i < 20; i++) {
+        System.out.printf("%4d │ %5d │%8d\n", numero[i], cuadrado[i], cubo[i]);
+      }
+  }
 }
