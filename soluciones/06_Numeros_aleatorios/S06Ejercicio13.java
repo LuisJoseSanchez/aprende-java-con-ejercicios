8b6ced69118800f377eff745260cb1d8555c6c26
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:19:09 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio13.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio13.java
@@ -1,13 +1,11 @@
 /**
- * 
  * 6. Números aleatorios
  * 
  * 13. Escribe un programa que simule la tirada de dos dados. El programa deberá
- *     continuar tirando los dados una y otra vez hasta que en alguna tirada los dos dados
- *     tengan el mismo valor.
+ *     continuar tirando los dados una y otra vez hasta que en alguna tirada los
+ *     dos dados tengan el mismo valor.
  *
  * @author Luis José Sánchez
- * 
  */
 
 public class S06Ejercicio13 {
