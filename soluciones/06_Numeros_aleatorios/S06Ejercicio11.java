ff6785db807928d3ee46c5e875b7f32121afcf2c
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:04:46 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio11.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio11.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 6. Números aleatorios
  * 
  * 11. Escribe un programa que muestre 20 notas generadas al azar. Las notas deben aparecer de la
@@ -7,7 +6,6 @@
  *     suspensos, el número de suficientes, el número de bienes, etc.
  *
  * @author Luis José Sánchez
- * 
  */
 
 public class S06Ejercicio11 {
@@ -15,27 +13,38 @@ public class S06Ejercicio11 {
   public static void main(String[] args) {
     
     int nota;
-    int suspensos = 0, suficientes = 0, bienes = 0, notables = 0, sobresalientes = 0;
+    int suspensos = 0;
+    int suficientes = 0;
+    int bienes = 0;
+    int notables = 0;
+    int sobresalientes = 0;
     
     for(int i = 0; i < 20; i++) {
       
       nota = (int)(Math.random() * 5);
       
       switch(nota) {
-        case 0: System.out.print("suspenso ");
-            suspensos++;
-            break;
-        case 1: System.out.print("suficiente ");
-            suficientes++;
-            break;
-        case 2: System.out.print("bien ");
-            bienes++;
-            break;
-        case 3: System.out.print("notable ");
-            notables++;
-            break;
-        case 4: System.out.print("sobresaliente ");
-            sobresalientes++;
+        case 0:
+          System.out.print("suspenso ");
+          suspensos++;
+          break;
+        case 1:
+          System.out.print("suficiente ");
+          suficientes++;
+          break;
+        case 2:
+          System.out.print("bien ");
+          bienes++;
+          break;
+        case 3:
+          System.out.print("notable ");
+          notables++;
+          break;
+        case 4:
+          System.out.print("sobresaliente ");
+          sobresalientes++;
+          break;
+        default:
       }
 
     }
