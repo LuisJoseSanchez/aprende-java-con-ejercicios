90137a60361a02603bf1df5ba232a09267ebb39a
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:37:38 +0100

    7.4.1 Arrays de una dimensión. Revisando soluciones.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio01.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio01.java
@@ -1,14 +1,15 @@
 /**
  * 7.1 Arrays
  * 
- * 1. Define un array de 12 números enteros con nombre “num” y asigna los
- *    valores según la siguiente tabla:
+ * 1. Define un array de 12 números enteros con nombre num y asigna los valores
+ *    según la tabla que se muestra a continuación. Muestra el contenido de todos
+ *    los elementos del array. ¿Qué sucede con los valores de los elementos que
+ *    no han sido inicializados?
  *    Posición  0   1   2   3   4   5   6   7   8   9   10   11
  *    Valor    39  -2           0      14       5  120
  *
  * @author Luis José Sánchez
  */
-
 public class S71Ejercicio01 {
 
 	public static void main(String[] args) {
@@ -22,14 +23,10 @@ public class S71Ejercicio01 {
 			num[8] = 5;
 			num[9] = 120;
 						
-			for (int i = 1; i < 12; i++)
-				System.out.println(num[i]);
-/**
- * 
- * Se puede observar que los valores del array se inicializan
- * automáticamente a 0.
- *
- */			
-
+			for (int i = 0; i < 12; i++) {
+				System.out.printf("num[%2d]: %d\n", i, num[i]);
+      }
+      
+  // Se puede observar que los valores del array se inicializan automáticamente a 0.
 	}
 }
