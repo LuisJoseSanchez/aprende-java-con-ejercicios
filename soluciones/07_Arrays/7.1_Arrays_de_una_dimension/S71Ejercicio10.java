1891cbcdf7c5101f852c40a96d9de852cfd2f822
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 10:57:59 +0100

    7.4.1 Arrays de una dimensión. Revisando soluciones.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio10.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio10.java
@@ -8,32 +8,43 @@
  *
  * @author Luis José Sánchez
  */
-
 public class S71Ejercicio10 {
 
-	public static void main(String[] args) {
-	
-		int[] n     = new int[20];
-		int[] par   = new int[20];
-		int[] impar = new int[20];
-		int i, pares = 0, impares = 0;
-		
-		for (i = 0; i < 20; i++) {
-			n[i] = (int)(Math.random()*101);
-			if (n[i] % 2 == 0)
-					par[pares++] = n[i];
-				else
-					impar[impares++] = n[i];
-		}
-		
-		System.out.println("Array original:");
-		for (i = 0; i < 20; i++) System.out.print(n[i] + " ");
-		System.out.println();
-		
-		for (i = 0; i < pares; i++) n[i] = par[i];
-		for (i = pares; i < 20; i++) n[i] = impar[i-pares];
+  public static void main(String[] args) {
+  
+    int[] n = new int[20];
+    int[] par = new int[20];
+    int[] impar = new int[20];
+    int i;
+    int pares = 0;
+    int impares = 0;
+    
+    for (i = 0; i < 20; i++) {
+      n[i] = (int)(Math.random() * 101);
+      if (n[i] % 2 == 0) {
+        par[pares++] = n[i];
+      } else {
+        impar[impares++] = n[i];
+      }
+    }
+    
+    System.out.println("Array original:");
+    for (i = 0; i < 20; i++) {
+      System.out.print(n[i] + " ");
+    }
+    System.out.println();
+    
+    for (i = 0; i < pares; i++) {
+      n[i] = par[i];
+    }
+    
+    for (i = pares; i < 20; i++) {
+      n[i] = impar[i - pares];
+    }
 
-		System.out.println("Array con los pares al principio:");
-		for (i = 0; i < 20; i++) System.out.print(n[i] + " ");
-	}
+    System.out.println("Array con los pares al principio:");
+    for (i = 0; i < 20; i++) {
+      System.out.print(n[i] + " ");
+    }
+  }
 }
--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio11.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio11.java
@@ -12,49 +12,49 @@
 
 public class S71Ejercicio11 {
 
-	public static void main(String[] args) {
-	
-		int[] n       = new int[10];
-		int[] primo   = new int[10];
-		int[] noPrimo = new int[10];
-		int i, j, primos = 0, noPrimos = 0;
-		boolean esPrimo = false;
-		
-		System.out.println("Introduzca 10 números separados por INTRO:");
+  public static void main(String[] args) {
+  
+    int[] n       = new int[10];
+    int[] primo   = new int[10];
+    int[] noPrimo = new int[10];
+    int i, j, primos = 0, noPrimos = 0;
+    boolean esPrimo = false;
+    
+    System.out.println("Introduzca 10 números separados por INTRO:");
 
-		for (i = 0; i < 10; i++) {
-			n[i] = Integer.parseInt(System.console().readLine());
-			
-			// comprueba si el número introducido es o no primo
-			esPrimo = true;
-			for (j = 2; j < n[i] - 1; j++)
-				if (n[i] % j == 0) esPrimo = false;
-			
-			if (esPrimo)
-					primo[primos++] = n[i];
-				else
-					noPrimo[noPrimos++] = n[i];
-		}
-		
-		System.out.println("\n\nArray original:");
-		System.out.println("\n┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
-		System.out.print("│ Índice ");
-		for (i = 0; i < 10; i++) System.out.printf("│%4d ", i);
-		System.out.println("│\n├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");		
-		System.out.print("│ Valor  ");
-		for (i = 0; i < 10; i++) System.out.printf("│%4d ", n[i]);
-		System.out.println("│\n└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
-		
-		for (i = 0; i < primos; i++) n[i] = primo[i];
-		for (i = primos; i < primos + noPrimos - 1; i++) n[i] = noPrimo[i-primos];
+    for (i = 0; i < 10; i++) {
+      n[i] = Integer.parseInt(System.console().readLine());
+      
+      // comprueba si el número introducido es o no primo
+      esPrimo = true;
+      for (j = 2; j < n[i] - 1; j++)
+        if (n[i] % j == 0) esPrimo = false;
+      
+      if (esPrimo)
+          primo[primos++] = n[i];
+        else
+          noPrimo[noPrimos++] = n[i];
+    }
+    
+    System.out.println("\n\nArray original:");
+    System.out.println("\n┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+    System.out.print("│ Índice ");
+    for (i = 0; i < 10; i++) System.out.printf("│%4d ", i);
+    System.out.println("│\n├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");    
+    System.out.print("│ Valor  ");
+    for (i = 0; i < 10; i++) System.out.printf("│%4d ", n[i]);
+    System.out.println("│\n└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
+    
+    for (i = 0; i < primos; i++) n[i] = primo[i];
+    for (i = primos; i < primos + noPrimos - 1; i++) n[i] = noPrimo[i-primos];
 
-		System.out.println("\n\nArray con los primos al principio:");
-		System.out.println("\n┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
-		System.out.print("│ Índice ");
-		for (i = 0; i < 10; i++) System.out.printf("│%4d ", i);
-		System.out.println("│\n├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");		
-		System.out.print("│ Valor  ");
-		for (i = 0; i < 10; i++) System.out.printf("│%4d ", n[i]);
-		System.out.println("│\n└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
-	}
+    System.out.println("\n\nArray con los primos al principio:");
+    System.out.println("\n┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+    System.out.print("│ Índice ");
+    for (i = 0; i < 10; i++) System.out.printf("│%4d ", i);
+    System.out.println("│\n├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");    
+    System.out.print("│ Valor  ");
+    for (i = 0; i < 10; i++) System.out.printf("│%4d ", n[i]);
+    System.out.println("│\n└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
+  }
 }
