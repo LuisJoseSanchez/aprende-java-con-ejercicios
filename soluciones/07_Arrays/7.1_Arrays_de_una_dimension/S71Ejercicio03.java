c4f7e94bfe2217142e0e5df1f9ab4d1764ae1d37
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:47:26 +0100

    7.4.1 Arrays de una dimensión. Revisando soluciones.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio03.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio03.java
@@ -7,22 +7,23 @@
  *
  * @author Luis José Sánchez
  */
-
 public class S71Ejercicio03 {
 
-	public static void main(String[] args) {
-		
-			int[] n = new int[10];
-			int i;
-			
-			System.out.println("Por favor, introduzca 10 números enteros.");
-			System.out.println("Pulse ENTRAR después de introducir cada número.");
+  public static void main(String[] args) {
+    
+    int[] n = new int[10];
+    int i;
+    
+    System.out.println("Por favor, introduzca 10 números enteros.");
+    System.out.println("Pulse la tecla INTRO después de introducir cada número.");
 
-			for (i = 0; i < 10; i++)
-				n[i] = Integer.parseInt(System.console().readLine());
-						
-			System.out.println("\nLos números introducidos, al revés, son los siguientes:");
-			for (i = 9; i >= 0; i--)
-				System.out.println(n[i]);
-	}
+    for (i = 0; i < 10; i++) {
+      n[i] = Integer.parseInt(System.console().readLine());
+    }
+    
+    System.out.println("\nLos números introducidos, al revés, son los siguientes:");
+    for (i = 9; i >= 0; i--) {
+      System.out.println(n[i]);
+    }
+  }
 }
