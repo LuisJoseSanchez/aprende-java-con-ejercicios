51cf549b2fc314d0cbef9ed74fb7bb1ef6b5d96e
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 08:51:49 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio09.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio09.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 6. Números aleatorios
  * 
  * 9. Realiza un programa que vaya generando números aleatorios pares
@@ -7,11 +6,8 @@
  *    programa deberá decir al final cuántos números se han generado.
  *
  * @author Luis José Sánchez
- * 
  */
-
 public class S06Ejercicio09 {
-
   public static void main(String[] args) {
     
     int n = 0;
@@ -22,7 +18,6 @@ public class S06Ejercicio09 {
       System.out.print(n + " ");
       cuentaNumeros++;
     }
-
     System.out.println("\nSe han generado " + cuentaNumeros + " números.");
   }
 }
