35d58bbf3d84fc9c8f8c4606b27ba537b749aaf6
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:17:38 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio12.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio12.java
@@ -1,5 +1,4 @@
 /**
- * 
  * 6. Números aleatorios
  * 
  * 12. Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix) con el
@@ -7,16 +6,24 @@
  *     entero en un carácter.
  *
  * @author Luis José Sánchez
- * 
  */
-
 public class S06Ejercicio12 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+    throws InterruptedException {
 
-    for(int i = 0; i < 4000; i++)
-      System.out.print((char)(Math.random() * ((126 - 32 + 1) + 32)));
-
-    System.out.println();
+    int linea = 0;
+    
+    System.out.print("\033[32m"); // pinta en verde
+    
+    for(int i = 0; i < 8000; i++) {
+      System.out.print((char)(Math.random() * (126 - 32 + 1) + 32));
+      
+      if (linea++ == 60) {
+        linea = 0;
+        Thread.sleep(50);
+        System.out.println();
+      }
+    }
   }
 }
