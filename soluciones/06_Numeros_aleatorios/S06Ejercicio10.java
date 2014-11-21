70ad5910fb60a486958ade2647f002d9745a9007
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-21 09:01:55 +0100

    Revisando las soluciones del capítulo 6.

--- soluciones/06_Numeros_aleatorios/S06Ejercicio10.java
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio10.java
@@ -1,20 +1,19 @@
 /**
- * 
  * 6. Números aleatorios
  * 
- * 10. Realiza un programa que pinte por pantalla diez líneas formadas por caracteres. Las líneas
- *     deben tener una longitud aleatoria entre 1 y 40 caracteres. Estos caracteres podrán ser uno
- *     de los siguientes: *, -, =, ., |, @
+ * 10. Realiza un programa que pinte por pantalla diez líneas formadas por caracteres.
+ *     El carácter con el que se pinta cada línea se elige de forma aleatoria entre
+ *     uno de los siguientes: *, -, =, ., |, @. Las líneas deben tener una longitud
+ *     aleatoria entre 1 y 40 caracteres.
  *
  * @author Luis José Sánchez
- * 
  */
-
 public class S06Ejercicio10 {
 
   public static void main(String[] args) {
     
-    int r, longitud;
+    int r;
+    int longitud;
     String relleno = "";
     
     for(int i = 1; i <= 10; i++) {
@@ -23,18 +22,31 @@ public class S06Ejercicio10 {
       r = (int)(Math.random() * 6);
       
       switch(r) {
-        case 0: relleno = "*"; break;
-        case 1: relleno = "-"; break;
-        case 2: relleno = "="; break;
-        case 3: relleno = "."; break;
-        case 4: relleno = "|"; break;
-        case 5: relleno = "@";
+        case 0:
+          relleno = "*";
+          break;
+        case 1:
+          relleno = "-";
+          break;
+        case 2:
+          relleno = "=";
+          break;
+        case 3:
+          relleno = ".";
+          break;
+        case 4:
+          relleno = "|";
+          break;
+        case 5:
+          relleno = "@";
+          break;
+        default:
       }
       
       // pinta la línea
-      for(int j = 1; j <= longitud; j++)
+      for(int j = 1; j <= longitud; j++) {
         System.out.print(relleno);
-
+      }
       System.out.println();
     }
   }
