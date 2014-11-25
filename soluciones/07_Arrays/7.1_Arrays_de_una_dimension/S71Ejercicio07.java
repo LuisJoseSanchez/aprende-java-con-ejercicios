3e8d69d72f3a5e83878b1009eaaf937c8d6924d9
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-11-25 09:05:47 +0100

    Tabulaciones y comentarios.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio07.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio07.java
@@ -12,32 +12,33 @@ public class S71Ejercicio07 {
 
   public static void main(String[] args) {
     
-      int[] numero = new int[100];
-      int i;
-      String verde = "\033[32m";
-      String blanco = "\033[37m";
-            
-      for (i = 0; i < 100; i++) {
-        numero[i] = (int)(Math.random() * 21);
-        System.out.print(numero[i] + "  ");
-      }
-      
-      System.out.print("\nIntroduzca un número de los que se han mostrado: ");
-      int valor1 = Integer.parseInt(System.console().readLine());
-      System.out.print("Introduzca el valor por el que quiere sustituirlo: ");
-      int valor2 = Integer.parseInt(System.console().readLine());
+    int[] numero = new int[100];
+    int i;
+    String verde = "\033[32m";
+    String blanco = "\033[37m";
+    
+    // Rellena el array con números aleatorios
+    for (i = 0; i < 100; i++) {
+      numero[i] = (int)(Math.random() * 21);
+      System.out.print(numero[i] + "  ");
+    }
+    
+    System.out.print("\nIntroduzca un número de los que se han mostrado: ");
+    int valor1 = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca el valor por el que quiere sustituirlo: ");
+    int valor2 = Integer.parseInt(System.console().readLine());
 
-      System.out.println();
-      
-      for (i = 0; i < 100; i++) {
-        if (numero[i] == valor1) {
-          numero[i] = valor2;
-          System.out.print(verde + "\"" + numero[i] + "\"  ");
-        } else {
-          System.out.print(blanco + numero[i] + "  ");
-        }
+    System.out.println();
+    
+    for (i = 0; i < 100; i++) {
+      if (numero[i] == valor1) {
+        numero[i] = valor2;
+        System.out.print(verde + "\"" + numero[i] + "\"  ");
+      } else {
+        System.out.print(blanco + numero[i] + "  ");
       }
+    }
 
-      System.out.println();
+    System.out.println();
   }
 }
