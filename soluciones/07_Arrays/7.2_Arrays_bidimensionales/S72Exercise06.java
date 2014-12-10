aa7002678830b746b306144f4bb252914e5b79ab
LuisJoseSanchez <luisjoseprofe@gmail.com>
2014-12-10 12:04:25 +0100

    Revisando ejercicios del capítulo 7 - Arrays bidimensionales.

--- soluciones/07_Arrays/7.2_Arrays_bidimensionales/S72Exercise06.java
+++ soluciones/07_Arrays/7.2_Arrays_bidimensionales/S72Exercise06.java
@@ -1,70 +1,89 @@
 /**
- * 
  * 7.2 Arrays bidimensionales
  * 
  * 6. Modica el programa anterior de tal forma que no se repita ningún número en el array.
  * 
  * @author Luis José Sánchez
- * 
  */
-
 public class S72Exercise06 {
 
-	public static void main(String[] args) 
-		throws InterruptedException {
+  public static void main(String[] args) 
+    throws InterruptedException {
  
-		int fila, columna, i, j, filaMaximo = 0, columnaMaximo = 0, filaMinimo = 0, columnaMinimo = 0;
-		int	maximo = Integer.MIN_VALUE, minimo = Integer.MAX_VALUE;
-		int repeticiones = 666;
-		int[][] num = new int[6][10];
-		boolean arrayValida = true;
-		
-		// Introduce valores aleatorios en el array de tal forma que no se repita ninguno
-		do {
-			arrayValida = true;
-			
-			for(fila = 0; fila < 6; fila++)
-				for(columna = 0; columna < 10; columna++)
-					num[fila][columna] = (int)(Math.random() * 1001);
+    int[][] num = new int[6][10];
 
-			for(fila = 0; fila < 6; fila++)
-				for(columna = 0; columna < 10; columna++) {
-					repeticiones = 0;
-					
-					for(i = 0; i < 6; i++)
-						for(j = 0; j < 10; j++)
-							if (num[fila][columna] == num[i][j])
-								repeticiones++;
-								
-					if (repeticiones > 1)
-						arrayValida = false;
-				}
-				
+    int fila;
+    int columna;
 
-						
-		} while (!arrayValida);
-		// Calcula el máximo, el mínimo y las posiciones que ocupan
-		for(fila = 0; fila < 6; fila++) {
-			for(columna = 0; columna < 10; columna++) {
-				System.out.printf("%7d   ", num[fila][columna]);
-				Thread.sleep(100);
-				
-				if (num[fila][columna] < minimo) {
-					minimo = num[fila][columna];
-					filaMinimo = fila;
-					columnaMinimo = columna;
-				}
-				
-				if (num[fila][columna] > maximo) {
-					maximo = num[fila][columna];
-					filaMaximo = fila;
-					columnaMaximo = columna;
-				}
-			}
-			System.out.println();
-		}
+    // Genera el contenido del array sin que se repita ningún valor
+    boolean repetido;
+    int i;
+    int j;
+      
+    for(fila = 0; fila < 6; fila++) {
+      for(columna = 0; columna < 10; columna++) {
+        do {
+          num[fila][columna] = (int)(Math.random() * 1001);
 
-		System.out.println("El máximo es " + maximo + " y está en la fila " + filaMaximo + ", columna " + columnaMaximo);
-		System.out.println("El mínimo es " + minimo + " y está en la fila " + filaMinimo + ", columna " + columnaMinimo);
+          // Comprueba si el número generado ya está en el array.
+          repetido = false;
+          for (i = 0; i < 10 * fila + columna; i++) {
+            if (num[fila][columna] == num[i / 10][i % 10]) {
+              repetido = true;
+            }
+          }
+        } while (repetido);
+      }
     }
+
+    int minimo = Integer.MAX_VALUE;
+    int filaMinimo = 0;
+    int columnaMinimo = 0;
+    
+    int maximo = Integer.MIN_VALUE;
+    int filaMaximo = 0;
+    int columnaMaximo = 0;
+
+    System.out.print("\n      ");
+    for(columna = 0; columna < 10; columna++) {
+      System.out.print("   " + columna + "  ");
+    }
+    System.out.println();
+    
+    System.out.print("    ┌");
+    for(columna = 0; columna < 10; columna++) {
+      System.out.print("──────");
+    }
+    System.out.println("┐");
+    
+    for(fila = 0; fila < 6; fila++) {
+      System.out.print("  " + fila + " │");
+      for(columna = 0; columna < 10; columna++) {
+        System.out.printf("%5d ", num[fila][columna]);
+        Thread.sleep(100);
+        
+        // Calcula el mínimo y guarda sus coordenadas
+        if (num[fila][columna] < minimo) {
+          minimo = num[fila][columna];
+          filaMinimo = fila;
+          columnaMinimo = columna;
+        }
+        
+        // Calcula el máximo y guarda sus coordenadas
+        if (num[fila][columna] > maximo) {
+          maximo = num[fila][columna];
+          filaMaximo = fila;
+          columnaMaximo = columna;
+        }
+      }
+      System.out.println("│");
+    }
+    System.out.print("    └");
+    for(columna = 0; columna < 10; columna++) {
+      System.out.print("──────");
+    }
+
+    System.out.println("┘\n\nEl máximo es " + maximo + " y está en la fila " + filaMaximo + ", columna " + columnaMaximo);
+    System.out.println("El mínimo es " + minimo + " y está en la fila " + filaMinimo + ", columna " + columnaMinimo);
+  }
 }
