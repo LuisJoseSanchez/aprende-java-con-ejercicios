9f22741409c10dec4ef69579274e7df29cc42f9e
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-01-26 08:32:46 +0100

    Actualizando los ejemplos del capítulo 8 - Funciones

--- ejemplos/08_Funciones/PruebaParametros1.java
+++ ejemplos/08_Funciones/PruebaParametros1.java
@@ -5,19 +5,19 @@
  */
  
 public class PruebaParametros1 {
-	
-	public static void main(String[] args) {
-		
-		int n = 10;
-		
-		System.out.println(n);
-		calcula(n);
-		System.out.println(n);
-	}
-	
-	public static void calcula(int x) {
-			
-			x += 24;
-			System.out.println(x);
-	}
+  
+  public static void main(String[] args) {
+    
+    int n = 10;
+    
+    System.out.println(n);
+    calcula(n);
+    System.out.println(n);
+  }
+  
+  public static void calcula(int x) {
+      
+      x += 24;
+      System.out.println(x);
+  }
 }
