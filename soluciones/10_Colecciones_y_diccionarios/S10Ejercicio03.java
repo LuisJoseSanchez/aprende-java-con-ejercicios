66875b6d2673aeb684f391ecb1af48d29fc0a2d4
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-04-14 10:47:36 +0200

    Revisando soluciones del capítulo 10: Colecciones y diccionarios

--- soluciones/10_Colecciones_y_diccionarios/S10Ejercicio03.java
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio03.java
@@ -4,32 +4,36 @@
  *
  * @author Luis José Sánchez
  */
- 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class S10Ejercicio03 {
-   public static void main(String[] args) {
-		
-   		ArrayList<Integer> a = new ArrayList<Integer>();
-   		
-   		System.out.println("Introduzca 10 números enteros: ");
-        for (int i = 0; i < 10; i++)
-			a.add(Integer.parseInt(System.console().readLine()));    
-		
-		System.out.println("Lista original: " + a);
-        
-        Collections.sort(a);
-        
-        System.out.println("Lista ordenada de menor a mayor: " + a);
+  public static void main(String[] args) {
 
-		// Descomentar el siguiente bloque para ordenar la lista de mayor a menor
-        /*
-        ArrayList<Integer> b = new ArrayList<Integer>();
+    ArrayList<Integer> a = new ArrayList<Integer>();
 
-        for(int n : a)
-			b.add(0, n);
+    System.out.println("Introduzca 10 números enteros: ");
+    
+    for (int i = 0; i < 10; i++) {
+      a.add(Integer.parseInt(System.console().readLine()));
+    }   
+
+    System.out.println("Lista original: " + a);
+
+    Collections.sort(a);
+
+    System.out.println("Lista ordenada de menor a mayor: " + a);
+
+    // Descomentar el siguiente bloque para ordenar la lista de mayor a menor
+    /*
+      ArrayList<Integer> b = new ArrayList<Integer>();
+
+      for(int n : a) {
+        b.add(0, n);
+      }
         
-        System.out.print("Lista ordenada de mayor a menor: " + b);
-		*/
-     }
+      System.out.print("Lista ordenada de mayor a menor: " + b);
+    */
+  }
 }
