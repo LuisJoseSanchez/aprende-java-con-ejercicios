f4fc5d2486010c2278a5dce850cc9953139f6635
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-04-26 18:21:52 +0200

    Revisando soluciones del capítulo 10: Colecciones y diccionarios

--- soluciones/10_Colecciones_y_diccionarios/S10Ejercicio07/Eurocoin.java
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio07/Eurocoin.java
@@ -16,33 +16,33 @@
  *    10 céntimos – cara
  *    
  *    @author Luis José Sánchez
- *    
  */
 
 import java.util.ArrayList;
 
 public class Eurocoin {
 
-	public static void main(String[] args) {
-		ArrayList<Moneda> m = new ArrayList<Moneda>();
-		
-		Moneda monedaAux = new Moneda();
-		m.add(monedaAux);
-		
-		String ultimaPosicion = monedaAux.getPosicion();
-		String ultimaCantidad = monedaAux.getCantidad();
-		
-		for (int i = 0; i < 5; i++) {
-			do {
-				monedaAux = new Moneda();
-			} while (!((monedaAux.getPosicion()).equals(ultimaPosicion)) && !((monedaAux.getCantidad()).equals(ultimaCantidad)));
-			
-			m.add(monedaAux);
-			ultimaPosicion = monedaAux.getPosicion();
-			ultimaCantidad = monedaAux.getCantidad();
-		}
-		
-		for (Moneda mo : m)
-			System.out.println(mo);
-	}
+  public static void main(String[] args) {
+    ArrayList<Moneda> m = new ArrayList<Moneda>();
+    
+    Moneda monedaAux = new Moneda();
+    m.add(monedaAux);
+    
+    String ultimaPosicion = monedaAux.getPosicion();
+    String ultimaCantidad = monedaAux.getCantidad();
+    
+    for (int i = 0; i < 5; i++) {
+      do {
+        monedaAux = new Moneda();
+      } while (!((monedaAux.getPosicion()).equals(ultimaPosicion)) && !((monedaAux.getCantidad()).equals(ultimaCantidad)));
+      
+      m.add(monedaAux);
+      ultimaPosicion = monedaAux.getPosicion();
+      ultimaCantidad = monedaAux.getCantidad();
+    }
+    
+    for (Moneda mo : m) {
+      System.out.println(mo);
+    }
+  }
 }
