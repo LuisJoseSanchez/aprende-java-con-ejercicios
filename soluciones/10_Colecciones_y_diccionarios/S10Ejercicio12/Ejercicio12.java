99ff74b1e773e51b7511a04784f7f5c7923ba399
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-04-26 18:33:47 +0200

    Revisando soluciones del capítulo 10: Colecciones y diccionarios

--- soluciones/10_Colecciones_y_diccionarios/S10Ejercicio12/Ejercicio12.java
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio12/Ejercicio12.java
@@ -23,52 +23,52 @@ import java.util.HashMap;
 
 public class Ejercicio12 {
 
-	public static void main(String[] args) {
-		
-		ArrayList<Carta> c = new ArrayList<Carta>();
-		HashMap<String, Integer> h = new HashMap<String, Integer>();
-		int puntos = 0;
-		
-		h.put("as", 11);
-		h.put("dos", 0);
-		h.put("tres", 10);
-		h.put("cuatro", 0);
-		h.put("cinco", 0);
-		h.put("seis", 0);
-		h.put("siete", 0);
-		h.put("sota", 2);
-		h.put("caballo", 3);
-		h.put("rey", 4);
-			
-		Carta cartaAux = new Carta();
-		c.add(cartaAux);
-		
-		for (int i = 0; i < 5; i++) {
-			do {
-				cartaAux = new Carta();			
-			} while (existeCarta(cartaAux, c));
-			
-			c.add(cartaAux);
-		}
-		
-		
-		for (Carta miCarta: c) {
-			System.out.println(miCarta);
-			puntos += h.get(miCarta.getFigura());
-		}
-		
-		System.out.println("Tienes " + puntos + " puntos.");
-	} // main /////////////////////////////////////////////////////////
-	
-	
-	// Comprueba si existe una determinada carta dentro de un ArrayList
-	// de cartas
-	public static boolean existeCarta(Carta c, ArrayList<Carta> cartas) {
-		for (Carta car : cartas)
-			if ( (c.getNumero() == car.getNumero())
-				 && (c.getPalo().equals(car.getPalo())) )
-				 return true;
-				 
-		return false;
-	}
+  public static void main(String[] args) {
+    
+    ArrayList<Carta> c = new ArrayList<Carta>();
+    HashMap<String, Integer> h = new HashMap<String, Integer>();
+    int puntos = 0;
+    
+    h.put("as", 11);
+    h.put("dos", 0);
+    h.put("tres", 10);
+    h.put("cuatro", 0);
+    h.put("cinco", 0);
+    h.put("seis", 0);
+    h.put("siete", 0);
+    h.put("sota", 2);
+    h.put("caballo", 3);
+    h.put("rey", 4);
+      
+    Carta cartaAux = new Carta();
+    c.add(cartaAux);
+    
+    for (int i = 0; i < 5; i++) {
+      do {
+        cartaAux = new Carta();      
+      } while (existeCarta(cartaAux, c));
+      
+      c.add(cartaAux);
+    }
+    
+    
+    for (Carta miCarta: c) {
+      System.out.println(miCarta);
+      puntos += h.get(miCarta.getFigura());
+    }
+    
+    System.out.println("Tienes " + puntos + " puntos.");
+  } // main /////////////////////////////////////////////////////////
+  
+  
+  // Comprueba si existe una determinada carta dentro de un ArrayList
+  // de cartas
+  public static boolean existeCarta(Carta c, ArrayList<Carta> cartas) {
+    for (Carta car : cartas) {
+      if ( (c.getNumero() == car.getNumero()) && (c.getPalo().equals(car.getPalo())) ) {
+         return true;
+      }
+    }
+    return false;
+  }
 }
