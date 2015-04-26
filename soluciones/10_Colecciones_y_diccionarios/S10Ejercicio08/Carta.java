1c8789df344b3c536e2f25fd2c515956de5ceae9
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-04-26 18:23:37 +0200

    Revisando soluciones del capítulo 10: Colecciones y diccionarios

--- soluciones/10_Colecciones_y_diccionarios/S10Ejercicio08/Carta.java
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio08/Carta.java
@@ -1,26 +1,26 @@
 
 public class Carta {
 
-	private static String[] n = {"as", "dos", "tres", "cuatro", "cinco", "seis", "siete", "sota", "caballo", "rey"};
-	private static String[] p = {"bastos", "copas", "espadas", "oros"};
-	
-	private String numero;
-	private String palo;
+  private static String[] n = {"as", "dos", "tres", "cuatro", "cinco", "seis", "siete", "sota", "caballo", "rey"};
+  private static String[] p = {"bastos", "copas", "espadas", "oros"};
+  
+  private String numero;
+  private String palo;
 
-	public Carta() {
-		this.numero = n[(int)(Math.random()*10)];
-		this.palo = p[(int)(Math.random()*4)];
-	}
-	
-	public String getNumero() {
-		return numero;
-	}
+  public Carta() {
+    this.numero = n[(int)(Math.random()*10)];
+    this.palo = p[(int)(Math.random()*4)];
+  }
+  
+  public String getNumero() {
+    return numero;
+  }
 
-	public String getPalo() {
-		return palo;
-	}
+  public String getPalo() {
+    return palo;
+  }
 
-	public String toString() {
-		return this.numero + " de " + this.palo;
-	}
+  public String toString() {
+    return this.numero + " de " + this.palo;
+  }
 }
