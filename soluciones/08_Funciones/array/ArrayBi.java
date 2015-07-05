2044fbb41e8e43d73eda4e8fcb233b812b0e09b1
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-05 21:13:04 +0200

    Revisando soluciones del Capítulo 8: Funciones.

--- soluciones/08_Funciones/array/ArrayBi.java
+++ soluciones/08_Funciones/array/ArrayBi.java
@@ -99,15 +99,24 @@ public class ArrayBi {
           
     return f;
   }  
-  
-  
+
+  /**
+   * Devuelve una columna (array unidimensional) de un array
+   * bidimensional que se pasa como parámetro.
+   * 
+   * @param x       array bidimiensional de números enteros
+   * @param columna número de la columna que se quiere extraer del array
+   *                <code>x</code>
+   * @return        columna en forma de array unidimensional extraida
+   *                del array <code>x</code>
+   */
   public static int[] columnaDeArrayBiInt(int x[][], int columna) {
-    
     int[] c = new int[x.length];
     
-    for (int f = 0; f < x.length; f++)
+    for (int f = 0; f < x.length; f++) {
       c[f] = x[f][columna];
-          
+    }
+
     return c;
   }
  
