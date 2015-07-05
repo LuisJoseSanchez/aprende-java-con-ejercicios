de682aa71df267d77ac9fe241fd8ef424e8a7fc6
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-05 21:10:20 +0200

    Revisando soluciones del Capítulo 8: Funciones.

--- soluciones/08_Funciones/array/ArrayBi.java
+++ soluciones/08_Funciones/array/ArrayBi.java
@@ -80,13 +80,22 @@ public class ArrayBi {
   }
 
 
-
+  /**
+   * Devuelve una fila (array unidimensional) de un array bidimensional
+   * que se pasa como parámetro.
+   * 
+   * @param x    array bidimiensional de números enteros
+   * @param fila número de la fila que se quiere extraer del array
+   *             <code>x</code>
+   * @return     fila en forma de array unidimensional extraida del
+   *             array <code>x</code>
+   */
   public static int[] filaDeArrayBiInt(int x[][], int fila) {
-    
     int[] f = new int[x[0].length];
     
-    for (int c = 0; c < x[0].length; c++)
+    for (int c = 0; c < x[0].length; c++) {
       f[c] = x[fila][c];
+    }
           
     return f;
   }  
