8465c2c18eaf23acf3447aeae7f44600cf926597
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-06 10:10:01 +0200

    Revisando soluciones del Capítulo 8: Funciones.

--- soluciones/08_Funciones/array/ArrayBi.java
+++ soluciones/08_Funciones/array/ArrayBi.java
@@ -120,51 +120,94 @@ public class ArrayBi {
     return c;
   }
  
-  
-  
+  /**
+   * Devuelve la fila y la columna (en un array con dos elementos) de la
+   * primera ocurrencia de un número dentro de un array bidimensional.
+   * Si el número no se encuentra en el array, la función devuelve -1.
+   * 
+   * @param x array bidimiensional de números enteros
+   * @param n número que se buscará dentro del array <code>x</code>
+   * @return  array unidimensional de dos elementos que indican la fila
+   *          y la columna donde se encuentra <code>n</code> o <code>
+   *          {-1, -1}</code> en caso de que <code>n</code> no se
+   *          encuentre en <code>x</code>
+   */  
   public static int[] coordenadasEnArrayBiInt(int x[][], int n) {
-    
-    for (int f = 0; f < x.length; f++)
-      for (int c = 0; c < x[0].length; c++)
+    for (int f = 0; f < x.length; f++) {
+      for (int c = 0; c < x[0].length; c++) {
         if (x[f][c] == n) {
           int[] coordenadas = {f, c};
           return coordenadas;
         }
-        
+      }
+    }
     int[] coordenadas = {-1, -1};      
     return coordenadas;
   }
 
-
-
+  /**
+   * Dice si un número que se encuentra en una posición determinada de
+   * una matriz (un array bidimensional) que se pasa como parámetro es o
+   * no punto de silla.
+   * <p>
+   * El punto de silla cumple la condición de ser el mínimo en su fila y
+   * máximo en su columna.
+   * 
+   * @param x array bidimiensional de números enteros
+   * @param i fila dentro del array <code>x</code>
+   * @param j columna dentro del array <code>x</code>
+   * @return  verdadero si el número que se encuentra en la fila <code>i
+   *          </code> y la columna <code>j</code> es el mínimo en su
+   *          fila y el máximo en su columna.
+   */
   public static boolean esPuntoDeSilla(int x[][], int i, int j) {
     
     int[] fila = filaDeArrayBiInt(x, i);
     int[] columna = columnaDeArrayBiInt(x, j);
     
-    if ((Array.minimoArrayInt(fila) == x[i][j]) && (Array.maximoArrayInt(columna) == x[i][j]))
-      return true;
-    else
-      return false;
+    return ((Array.minimoArrayInt(fila) == x[i][j])
+        && (Array.maximoArrayInt(columna) == x[i][j]));
   }
   
-
-
+  /**
+   * Devuelve un array que contiene una de las diagonales del array
+   * bidimensional que se pasa como parámetro.
+   * 
+   * @param x         array bidimiensional de números enteros
+   * @param fila      fila del número que marcará las dos posibles
+   *                  diagonales dentro del array <code>x</code>
+   * @param columna   columna del número que marcará las dos posibles
+   *                  diagonales dentro del array <code>x</code>
+   * @param direccion cadena de caracteres que indica cuál de las dos
+   *                  posibles diagonales se devolverá; la cadena <code>
+   *                  "nose"</code> indica que se elige la diagonal que
+   *                  va del noroeste hacia el sureste, mientras que la
+   *                  cadena <code>"neso"</code> indica que se elige la
+   *                  diagonal que va del noreste hacia el suroeste
+   * @return          array unidimensional que contiene una diagonal
+   *                  definida por un número determinado por <code>fila
+   *                  </code> y <code>columna</code> y una dirección
+   *                  determinada por el parámetro <code>direccion
+   *                  </code>
+   */
   public static int[] diagonal(int x[][], int fila, int columna, String direccion) {
     
     int elementos = 0, i, j;
     int[] diagonalAux = new int [1000];
     
-    for (i = 0; i < x.length; i++)
-      for (j = 0; j < x[0].length; j++)
+    for (i = 0; i < x.length; i++) {
+      for (j = 0; j < x[0].length; j++) {
         if ( (((columna - j) == (fila - i)) && (direccion.equals("nose")))
-          || (((columna - j) == (i - fila)) && (direccion.equals("neso"))) )
+          || (((columna - j) == (i - fila)) && (direccion.equals("neso"))) ) {
           diagonalAux[elementos++] = x[i][j];
-    
+        }
+      }
+    }
             
     int[] diagonal = new int[elementos];
-    for (j = 0; j < elementos; j++)
+    for (j = 0; j < elementos; j++) {
       diagonal[j] = diagonalAux[j];
+    }
       
     return diagonal;
   }
