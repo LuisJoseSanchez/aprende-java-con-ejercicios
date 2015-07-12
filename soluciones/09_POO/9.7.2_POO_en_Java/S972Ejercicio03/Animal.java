4403ddcc1c488867963645b3ec478f3a49e266df
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-12 12:20:19 +0200

    Revisando soluciones del Capítulo 9: Programación Orientada a Objetos.

--- soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio03/Animal.java
+++ soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio03/Animal.java
@@ -1,26 +1,24 @@
-
 public abstract class Animal {
 
-	private Sexo sexo;
+  private Sexo sexo;
 
-	public Animal () {
-		this.sexo = Sexo.MACHO;
-	}
+  public Animal () {
+    this.sexo = Sexo.MACHO;
+  }
 
-	public Animal (Sexo s) {
-		this.sexo = s;
-	}
+  public Animal (Sexo s) {
+    this.sexo = s;
+  }
 
-	public Sexo getSexo() {
-		return this.sexo;
-	}
+  public Sexo getSexo() {
+    return this.sexo;
+  }
 
-	public void duerme() {
-		System.out.println("Zzzzzzz");
-	}
-	
-	public void come(String comida) {
-		System.out.println("Estoy comiendo " + comida);
-	}
-	
+  public void duerme() {
+    System.out.println("Zzzzzzz");
+  }
+  
+  public void come(String comida) {
+    System.out.println("Estoy comiendo " + comida);
+  }
 }
--- soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio03/Lagarto.java
+++ soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio03/Lagarto.java
@@ -1,22 +1,20 @@
-
 public class Lagarto extends Animal {
 
-	public Lagarto() {
-	}
+  public Lagarto() {}
 
-	public Lagarto(Sexo s) {
-		super(s);
-	}
+  public Lagarto(Sexo s) {
+    super(s);
+  }
 
-	public void tomaElSol() {
-		System.out.println("Estoy tomando el Sol");
-	}
+  public void tomaElSol() {
+    System.out.println("Estoy tomando el Sol");
+  }
 
-	public void baniate() {
-		System.out.println("Me estoy dando un chapuzón");
-	}
+  public void baniate() {
+    System.out.println("Me estoy dando un chapuzón");
+  }
 
-	public void escondete() {
-		System.out.println("Me he escondido, ya no me puedes ver");
-	}
+  public void escondete() {
+    System.out.println("Me he escondido, ya no me puedes ver");
+  }
 }
