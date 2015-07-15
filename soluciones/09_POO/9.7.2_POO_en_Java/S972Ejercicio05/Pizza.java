02a7296b8807e1124045592b4047ded1085867af
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-15 11:18:51 +0200

    Revisando soluciones a los ejercicios del capítulo 9: Programación Orientada a Objetos.

--- soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio05/Pizza.java
+++ soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio05/Pizza.java
@@ -35,7 +35,15 @@ public class Pizza {
   public static int getTotalServidas() {
     return Pizza.totalServidas;
   }
-  
+
+  /**
+   * Cambia el estado de la pizza de <code>pedida</code> a <code>servida
+   * </code>.
+   * <p>
+   * En caso de que la pizza se hubiera servido ya y se intenta servir
+   * de nuevo, se muestra el mensaje <code>esa pizza ya se ha servido
+   * </code>.
+   */
   public void sirve() {
     if (this.estado.equals("pedida")) {
       this.estado = "servida";
