21cf80fec99442981071aa477f950d8c906997f9
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-15 11:28:30 +0200

    Revisando soluciones a los ejercicios del capítulo 9: Programación Orientada a Objetos.

--- soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio06/Tiempo.java
+++ soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio06/Tiempo.java
@@ -1,3 +1,18 @@
+/**
+ * Definición de la clase <code>Tiempo</code>.
+ * <p>
+ * Un objeto de la clase <code>Tiempo</code> se puede crear de dos
+ * maneras diferentes. Se pueden indicar las horas, los minutos y los
+ * segundos de la forma <code>new Tiempo(3, 20, 45)</code>, o bien se
+ * puede indicar únicamente la cantidad de segundos de la forma <code>
+ * new Tiempo(12045)</code> (ambos objetos definen el mismo intervalo
+ * de tiempo).
+ * <p>
+ * Internamente, la clase solo almacena los segundos totales. A partir
+ * de éstos, se puede deducir todo lo demás.
+ * 
+ * @author Luis José Sánchez
+ */
 public class Tiempo {
   
   private int segundos;
