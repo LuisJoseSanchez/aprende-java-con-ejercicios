dc6d8f9cf8cbac6c7505f3ada3aa3c126f80dc92
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-19 11:09:55 +0200

    Revisando soluciones a los ejercicios del capítulo 10: Colecciones y Diccionarios.

--- soluciones/10_Colecciones_y_diccionarios/S10Ejercicio02.java
+++ soluciones/10_Colecciones_y_diccionarios/S10Ejercicio02.java
@@ -1,12 +1,12 @@
 /**
- * 2. Realiza un programa que introduzca valores aleatorios (entre 0 y 100)
- *    en un ArrayList y que luego calcule la suma, la media, el máximo y el
- *    mínimo de esos números. El tamaño de la lista también será aleatorio
- *    y podrá oscilar entre 10 y 20 elementos ambos inclusive.
+ * 2. Realiza un programa que introduzca valores aleatorios (entre 0 y
+ *    100) en un ArrayList y que luego calcule la suma, la media, el
+ *    máximo y el mínimo de esos números. El tamaño de la lista también
+ *    será aleatorio y podrá oscilar entre 10 y 20 elementos ambos
+ *    inclusive.
  *
  * @author Luis José Sánchez
  */
-
 import java.util.ArrayList;
 
 public class S10Ejercicio02 {
