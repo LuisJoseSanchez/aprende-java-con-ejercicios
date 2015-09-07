36ce679444fefe7a143f94a4e7ed1c34acb349b5
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-09-07 11:48:10 +0200

    Revisando ejemplos del capítulo 3: Lectura de datos desde teclado.

---
+++ ejemplos/03_Lectura_de_datos_desde_teclado/LeeDatosScanner02.java
@@ -0,0 +1,19 @@
+/**
+* Lectura de datos desde teclado usando la clase Scanner
+*
+* @author Luis J. Sánchez
+*/
+
+import java.util.Scanner;
+
+public class LeeDatosScanner02 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduce tu nombre y tu edad separados por un espacio: ");
+    String nombre = s.next();
+    int edad = s.nextInt();
+    
+    System.out.println("Tu nombre es " + nombre + " y tu edad es " + edad);
+  }
+}
