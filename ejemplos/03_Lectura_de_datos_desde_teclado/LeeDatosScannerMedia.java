87e0bf66ddabe4c4bf476f84ab9111085cd3e1d3
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-09-07 11:56:51 +0200

    Revisando ejemplos del capítulo 3: Lectura de datos desde teclado.

---
+++ ejemplos/03_Lectura_de_datos_desde_teclado/LeeDatosScannerMedia.java
@@ -0,0 +1,22 @@
+/**
+* Lectura de datos desde teclado usando la clase Scanner
+*
+* @author Luis J. Sánchez
+*/
+
+import java.util.Scanner;
+
+public class LeeDatosScannerMedia {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduce tres números (pueden contener decimales) separados por espacios: ");
+    double x1 = s.nextDouble();
+    double x2 = s.nextDouble();
+    double x3 = s.nextDouble();
+    
+    double media = (x1 + x2 + x3) / 3;
+    
+    System.out.println("La media de esos tres números es " + media);
+  }
+}
