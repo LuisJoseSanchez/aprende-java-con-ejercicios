8a0d6e8301f65f11509a9fb289dd7f40b6980170
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-09-07 13:43:35 +0200

    Revisando ejemplos del capítulo 5: Bucles.

--- ejemplos/05_Bucles/EjemploFor.java
+++ ejemplos/05_Bucles/EjemploFor.java
@@ -3,12 +3,11 @@
  *
  * @author Luis José Sánchez
  */
-
 public class EjemploFor {
   public static void main(String[] args) {
         
     for (int i = 1; i < 11; i++) {
-        System.out.println(i);
+      System.out.println(i);
     }
   }
 }
