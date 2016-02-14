4332b7bc4ad56a71d7defbac7aef160e6e216404
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-02-14 21:20:55 +0100

    Actualizando soluciones del Capítulo 4: Sentencia condicional

--- soluciones/04_Sentencia_condicional/S04Ejercicio23.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio23.java
@@ -22,7 +22,7 @@
  *
  * @author Luis José Sánchez
  */
-public class S03Ejercicio12 {
+public class S04Ejercicio23 {
   public static void main(String[] args) {
 
     System.out.print("Introduzca la base imponible: ");
@@ -36,16 +36,53 @@ public class S03Ejercicio12 {
     
     // Calcula el IVA y el precio antes del descuento
     
+    int tipoIVANumerico = 0;
+    
     switch(tipoIVA) {
-      
+      case "general":
+        tipoIVANumerico = 21;
+        break;
+      case "reducido":
+        tipoIVANumerico = 10;
+        break;
+      case "superreducido":
+        tipoIVANumerico = 4;
+        break;
+      default:
+        System.out.print("El tipo de IVA introducido no es correcto.");
     }
     
-    double iva = 
+    double iva = baseImponible * tipoIVANumerico / 100;
+    double precioSinDescuento = baseImponible + iva;
     
     // Calcula el descuento
     
+    double descuento = 0;
+    
+    switch(codigoPromocional) {
+      case "nopro":
+        break;
+      case "mitad": // el precio se reduce a la mitad
+        descuento = precioSinDescuento / 2;
+        break;
+      case "meno5": // se descuentan 5 euros
+        descuento = 5;
+        break;
+      case "5porc": // se descuenta el 5%
+        descuento = precioSinDescuento * 0.05;
+        break;
+      default:
+        System.out.print("El código promocional introducido no es correcto.");
+    }
+    
     // Muestra el precio final del producto desglosado
     
-    System.out.println(kb + "Kb son " +  (kb / 1024) + "Mb.");
+    double total = precioSinDescuento - descuento;
+    
+    System.out.printf("Base imponible       %6.2f\n", baseImponible);
+    System.out.printf("IVA (%2d%%)            %6.2f\n", tipoIVANumerico, iva);
+    System.out.printf("Precio con IVA       %6.2f\n", precioSinDescuento);
+    System.out.printf("Cód. promo. (%s) -%6.2f\n", codigoPromocional, descuento);
+    System.out.printf("TOTAL                %6.2f", total);
   }
 }
