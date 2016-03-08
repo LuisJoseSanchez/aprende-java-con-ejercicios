4ffe8574ed93c83c9de44ae8403c4ad041a3047d
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-03-08 11:01:44 +0100

    Corregido error en generaArrayInt

--- soluciones/08_Funciones/array/Array.java
+++ soluciones/08_Funciones/array/Array.java
@@ -31,7 +31,7 @@ public class Array {
     int[] x = new int[n];
 
     for(int i = 0; i < n; i++) {
-      x[i] = (int)(Math.random()*(maximo - minimo) + minimo + 1);
+      x[i] = (int)(Math.random()*(maximo - minimo + 1) + minimo);
     }
       
     return x;
