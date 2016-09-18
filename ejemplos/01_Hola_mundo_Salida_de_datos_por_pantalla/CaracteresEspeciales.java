05d78e1403eb4ce1b785a6bea1f248fdf83657e8
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-09-18 17:13:11 +0200

    Actualizando ejemplos

---
+++ ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/CaracteresEspeciales.java
@@ -0,0 +1,12 @@
+/**
+ * Muestra caracteres especiales
+ *
+ * @author Luis J. Sánchez
+ */
+
+public class CaracteresEspeciales {
+  public static void main(String[] args) {
+    System.out.println("Cuando estoy programando ⌨ ");
+    System.out.println("estoy muy 😆 y me siento como un ♔.");
+  }
+}
