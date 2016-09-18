521275d8db5da43d08942b92c2fcd33b80674319
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-09-18 19:19:36 +0200

    Actualizando soluciones

--- soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio03.java
+++ soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio03.java
@@ -1,26 +1,24 @@
 /**
- * 
- * 1. Salida por pantalla
+ * 1. Salida por pantalla.
  *
- * 3. Escribe un programa que muestre por pantalla 10 palabras en inglés
- *    junto a su correspondiente traducción al castellano. Las palabras
- *    deben estar distribuidas en dos columnas y alineadas a la izquierda.
- *    Pista: Se puede insertar un tabulador mediante \t.
+ * Solución al ejercicio 3.
+ * 
+ * "Aprende Java con Ejercicios" (https://leanpub.com/aprendejava)
  *
  * @author Luis José Sánchez
  */
 
 public class S01Ejercicio03 {
   public static void main(String[] args) {
-    System.out.println("computer\tordenador\n");
-    System.out.println("student\talumno\\a");
-    System.out.println("cat\tgato");
-    System.out.println("penguin\tpingüino");
-    System.out.println("machine\tmáquina");
-    System.out.println("nature\tnaturaleza");
-    System.out.println("light\tluz");
-    System.out.println("green\tverde");
-    System.out.println("book\tlibro");
-    System.out.println("pyramid\tpirámide");
+    System.out.printf("%-10s %s\n", "computer", "ordenador");
+    System.out.printf("%-10s %s\n", "student", "alumno\\a");
+    System.out.printf("%-10s %s\n", "cat", "gato");
+    System.out.printf("%-10s %s\n", "penguin", "pingüino");
+    System.out.printf("%-10s %s\n", "machine", "máquina");
+    System.out.printf("%-10s %s\n", "nature", "naturaleza");
+    System.out.printf("%-10s %s\n", "light", "luz");
+    System.out.printf("%-10s %s\n", "green", "verde");
+    System.out.printf("%-10s %s\n", "book", "libro");
+    System.out.printf("%-10s %s\n", "pyramid", "pirámide");
   }
 }
