60cde5d29c7169ea2fc872d55c09178924fc9357
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-10-12 13:21:48 +0200

    Actualizando soluciones a los ejercicios del Capítulo 4: Sentencia condicional

--- soluciones/04_Sentencia_condicional/S04Ejercicio23.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio23.java
@@ -1,7 +1,9 @@
 /**
- * 3. Lectura de datos desde teclado
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava
+ * 
+ * Capítulo 4. Sentencia condicional.
  *
- * 12. Escribe un programa que calcule el precio final de un producto según su
+ * 23. Escribe un programa que calcule el precio final de un producto según su
  *     base imponible (precio antes de impuestos), el tipo de IVA aplicado (general,
  *     reducido o superreducido) y el código promocional. Los tipos de IVA general,
  *     reducido y superreducido son del 21%, 10% y 4% respectivamente. Los códigos
@@ -49,7 +51,7 @@ public class S04Ejercicio23 {
         tipoIVANumerico = 4;
         break;
       default:
-        System.out.print("El tipo de IVA introducido no es correcto.");
+        System.out.println("El tipo de IVA introducido no es correcto.");
     }
     
     double iva = baseImponible * tipoIVANumerico / 100;
@@ -72,7 +74,7 @@ public class S04Ejercicio23 {
         descuento = precioSinDescuento * 0.05;
         break;
       default:
-        System.out.print("El código promocional introducido no es correcto.");
+        System.out.println("El código promocional introducido no es correcto.");
     }
     
     // Muestra el precio final del producto desglosado
@@ -82,7 +84,7 @@ public class S04Ejercicio23 {
     System.out.printf("Base imponible       %6.2f\n", baseImponible);
     System.out.printf("IVA (%2d%%)            %6.2f\n", tipoIVANumerico, iva);
     System.out.printf("Precio con IVA       %6.2f\n", precioSinDescuento);
-    System.out.printf("Cód. promo. (%s) -%6.2f\n", codigoPromocional, descuento);
+    System.out.printf("Cód. promo. (%5s) -%6.2f\n", codigoPromocional, descuento);
     System.out.printf("TOTAL                %6.2f", total);
   }
 }
---
+++ soluciones/04_Sentencia_condicional/S04Ejercicio24.java
@@ -0,0 +1,76 @@
+/**
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava
+ * 
+ * Capítulo 4. Sentencia condicional.
+ * 
+ * Ejercicio 24
+ *
+ * @author Luis José Sánchez
+ */
+public class S04Ejercicio24 {
+  public static void main(String[] args) {
+
+    System.out.println("1 - Programador junior");
+    System.out.println("2 - Prog. senior");
+    System.out.println("3 - Jefe de proyecto");
+    System.out.print("Introduzca el cargo del empleado (1 - 3): ");
+    int cargo = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("¿Cuántos días ha estado de viaje visitando clientes? ");
+    int diasVisita = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduzca su estado civil (1 - Soltero, 2 - Casado): ");
+    int estadoCivil = Integer.parseInt(System.console().readLine());
+    
+    // Calcula el IVA y el precio antes del descuento
+    
+    double sueldoBase = 0;
+    
+    switch(cargo) {
+      case 1: // Programador junior
+        sueldoBase = 950;
+        break;
+      case 2: // Programador senior
+        tipoIVANumerico = 10;
+        break;
+      case "superreducido":
+        tipoIVANumerico = 4;
+        break;
+      default:
+        System.out.print("El tipo de IVA introducido no es correcto.");
+    }
+    
+    double iva = baseImponible * tipoIVANumerico / 100;
+    double precioSinDescuento = baseImponible + iva;
+    
+    // Calcula el descuento
+    
+    double descuento = 0;
+    
+    switch(codigoPromocional) {
+      case "nopro":
+        break;
+      case "mitad": // el precio se reduce a la mitad
+        descuento = precioSinDescuento / 2;
+        break;
+      case "meno5": // se descuentan 5 euros
+        descuento = 5;
+        break;
+      case "5porc": // se descuenta el 5%
+        descuento = precioSinDescuento * 0.05;
+        break;
+      default:
+        System.out.print("El código promocional introducido no es correcto.");
+    }
+    
+    // Muestra el precio final del producto desglosado
+    
+    double total = precioSinDescuento - descuento;
+    
+    System.out.printf("Base imponible       %6.2f\n", baseImponible);
+    System.out.printf("IVA (%2d%%)            %6.2f\n", tipoIVANumerico, iva);
+    System.out.printf("Precio con IVA       %6.2f\n", precioSinDescuento);
+    System.out.printf("Cód. promo. (%s) -%6.2f\n", codigoPromocional, descuento);
+    System.out.printf("TOTAL                %6.2f", total);
+  }
+}
