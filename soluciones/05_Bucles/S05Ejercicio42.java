bc84f8adb68e1bd41497adba56756f6adc98530a
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-10-14 20:53:38 +0200

    Actualizando soluciones a los ejercicios del Capítulo 5: Bucles.

--- soluciones/05_Bucles/S05Ejercicio42.java
+++ soluciones/05_Bucles/S05Ejercicio42.java
@@ -1,31 +1,36 @@
 /**
- * 5. Bucles
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava)
  * 
- * 22. Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos.
+ * Capítulo 5. Bucles.
+ * 
+ * Ejercicio 42
  * 
  * @author Luis José Sánchez
  */
-public class S05Ejercicio22 {
+public class S05Ejercicio42 {
 
   public static void main(String[] args) {
 
-    System.out.print("Números primos entre 2 y 100: ");
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
        
-    boolean esPrimo = true;
-    int n;
-    int i;
+    boolean esPrimo;
 
-    for (n = 2; n <= 100; n++) {
+    for (int n = numeroIntroducido; n < numeroIntroducido + 5; n++) {
 
       // comprueba si n es primo
       esPrimo = true;
-      for (i = 2; i < n; i++) {
-        if (n%i == 0) esPrimo = false;
+      for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+          esPrimo = false;
+        }
       }
 
-      // si n es primo, se muestra por pantalla
+      // muestra por pantalla si n es primo o no
       if (esPrimo) {
-        System.out.print(n + " ");
+        System.out.println(n + " es primo");
+      } else {
+        System.out.println(n + " no es primo");
       }
     }
     System.out.println();
