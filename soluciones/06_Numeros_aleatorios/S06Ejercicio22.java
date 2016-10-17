42ecc3f2e194d30840fcfda70a8583d6b43232d7
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-10-17 17:33:27 +0200

    Actualizando soluciones a los ejercicios del Capítulo 6: Números aleatorios.

---
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio22.java
@@ -0,0 +1,39 @@
+/**
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava)
+ * 
+ * Capítulo 6. Números aleatorios.
+ * 
+ * Ejercicio 22
+ * 
+ * @author Luis José Sánchez
+ */
+public class S06Ejercicio22 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca la longitud de la serpiente en ");
+    System.out.print("caracteres contando la cabeza: ");
+    int longitud = Integer.parseInt(System.console().readLine());
+    
+    // pinta la cabeza
+    System.out.println("            @");
+    
+    // pinta el cuerpo
+    int i;
+    int x = 13;
+        
+    while (longitud > 1) {
+      // suma -1, 0 o 1 a la variable x
+      x += (int)(Math.random() * 3) - 1;
+      
+      // pinta x - 1 espacios
+      for (i = 1; i < x; i++) {
+        System.out.print(" ");
+      }
+
+      // pinta el cuerpo
+      System.out.println("#");
+
+      longitud--;
+    }
+  }
+}
