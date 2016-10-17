a8396390aeb0ec1ddced36c86a144a901051f1bd
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-10-17 17:23:48 +0200

    Actualizando soluciones a los ejercicios del Capítulo 6: Números aleatorios.

---
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio20.java
@@ -0,0 +1,32 @@
+/**
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava)
+ * 
+ * Capítulo 6. Números aleatorios.
+ * 
+ * Ejercicio 20
+ * 
+ * @author Luis José Sánchez
+ */
+public class S06Ejercicio20 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, indique la capacidad de la cuba en litros: ");
+    int capacidad = Integer.parseInt(System.console().readLine());
+
+    // Rellena la cuba con unos litros aleatorios
+    // teniendo en cuenta no pasarse de la capacidad.
+    int litros = (int)(Math.random() * (capacidad + 1));  
+        
+    for (int i = 0; i < capacidad; i++) {
+      if (i < (capacidad - litros)) {
+        System.out.println("#    #");
+      } else {
+        System.out.println("#====#");
+      }
+    }
+        
+    System.out.println("######"); // fondo de la cuba
+    System.out.print("La cuba tiene una capacidad de " + capacidad);
+    System.out.print(" litros y contiene " + litros + " litros de agua.");
+  }
+}
