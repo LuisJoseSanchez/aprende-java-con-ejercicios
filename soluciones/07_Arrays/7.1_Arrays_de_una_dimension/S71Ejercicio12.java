fab2206222e8b9bd8d57c4f21b1041ccdb5d99c7
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-10-28 11:03:05 +0200

    Actualizando ejercicios del Capítulo 7: Arrays

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio12.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio12.java
@@ -1,13 +1,9 @@
 /**
- * 7.1 Arrays
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava)
  * 
- * 12. Realiza un programa que pida 10 números por teclado y que los almacene en un array.
- *     A continuación se mostrará el contenido de ese array junto al índice (0 – 9).
- *     Seguidamente el programa pedirá dos posiciones a las que llamaremos "inicial" y
- *     "final". Se debe comprobar que inicial es menor que final y que ambos números están
- *     entre 0 y 9. El programa deberá colocar el número de la posición inicial en la posición
- *     final, rotando el resto de números para que no se pierda ninguno. Al final se debe mostrar
- *     el array resultante.
+ * Capítulo 7. Arrays
+ * 
+ * Ejercicio 12
  *
  * @author Luis José Sánchez
  */
--- soluciones/07_Arrays/7.2_Arrays_bidimensionales/S72Exercise09.java
+++ soluciones/07_Arrays/7.2_Arrays_bidimensionales/S72Exercise09.java
@@ -1,10 +1,10 @@
 /**
  * 
+ * Aprende Java con Ejercicios (https://leanpub.com/aprendejava)
+ * 
  * 7.2 Arrays bidimensionales
- *
- * 9. Realiza un programa que sea capaz de rotar todos los elementos de una matriz
- *    cuadrada una posición en el sentido de las agujas del reloj. La matriz debe tener
- *    12 filas por 12 columnas y debe contener números generados al azar entre 0 y 100.
+ * 
+ * Ejercicio 9 
  * 
  * @author Luis José Sánchez
  * 
