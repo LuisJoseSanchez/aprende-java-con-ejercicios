9a48c5cb389999983a42074223a053c8272f135d
Luis José Sánchez <luisjose@espiral.io>
2017-10-13 09:53:13 +0200

    Corregida la constante PI.

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio09.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio09.java
@@ -6,22 +6,18 @@
  *
  * @author Luis José Sánchez
  */
-
 public class S03Ejercicio09 {
-  
-  // Las constantes se deben declarar antes del main
-  static final double pi = 3.141592654;
-  // En lugar de definir una constante se podría usar
-  // la constante predefinida Math.PI 
-  
   public static void main(String[] args) {
 
+    final double PI = 3.141592654;
+    // En lugar de definir una constante se podría usar
+    // la constante predefinida Math.PI
     System.out.println("Volumen de un cono");
     System.out.print("Por favor, introduzca la altura (cm): ");
     double h = Double.parseDouble(System.console().readLine());
     System.out.print("Introduzca el radio (cm): ");
     double r = Double.parseDouble(System.console().readLine());
-    double v = (1.0/3.0) * pi * r * r * h;
-    System.out.println("El volumen del cono es de " +  v + " cm3");
+    double v = (1.0/3.0) * PI * r * r * h;
+    System.out.println("El volumen del cono es de " +  v + " cm³");
   }
 }
