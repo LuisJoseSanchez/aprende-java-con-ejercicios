938f8b11b9531466075e15f49252f723aadc748c
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-05-02 11:08:33 +0200

    Actualizando ejemplos de control de excepciones.

--- ejemplos/14_Control_de_excepciones/EjemploExcepciones04.java
+++ ejemplos/14_Control_de_excepciones/EjemploExcepciones04.java
@@ -4,14 +4,26 @@ public class EjemploExcepciones04 {
 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    
-    System.out.println("Este programa pinta una línea de asteriscos");
-    
-    System.out.print("Introduzca la longitud de la línea (un número entero): ");
-    int longitud = Integer.parseInt(s.nextLine());
-    
-    for (int i = 0; i < longitud; i++) {
-      System.out.print("*");
+
+    System.out.println("Este programa pinta varias líneas de asteriscos");
+
+    System.out.print("Introduzca el número total de asteriscos: ");
+    int asteriscos = Integer.parseInt(s.nextLine());
+
+    System.out.print("Introduzca el número de líneas que quiere pintar: ");
+    int lineas = Integer.parseInt(s.nextLine());
+
+    int longitud = asteriscos / lineas;
+
+    int cuentaAsteriscos = 0;
+
+    for (int i = 1; i <= lineas; i++) {
+      System.out.print("Línea " + i + ": ");
+      for (int j = 0; (j < longitud) && (cuentaAsteriscos++ < asteriscos); j++) {
+        System.out.print("*");
+      }
+      System.out.println();
     }
+
   }
 }
