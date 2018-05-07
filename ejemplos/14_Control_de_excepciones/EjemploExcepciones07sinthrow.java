10b845946b3d595b28865f7628431fedbeb24932
Luis José Sánchez <luisjose@espiral.io>
2018-05-07 13:14:04 +0200

    Actualizando ejemplos de excepciones

---
+++ ejemplos/14_Control_de_excepciones/EjemploExcepciones07sinthrow.java
@@ -0,0 +1,7 @@
+public class EjemploExcepciones07sinthrow {
+
+  public static void main(String[] args) {
+    System.out.println("Inicio");
+    System.out.println(1 / 0);
+  }
+}
---
+++ ejemplos/14_Control_de_excepciones/EjemploExcepciones08manzanas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EjemploExcepciones08manzanas {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.print("Número de manzanas: ");
+    int m = Integer.parseInt(s.nextLine());
+    System.out.print("Número de personas: ");
+    int p = Integer.parseInt(s.nextLine());
+    System.out.print("A cada persona le corresponden " + reparteManzanas(m, p) + " manzanas.");
+  }
+  
+  public static int reparteManzanas(int manzanas, int personas) {
+    return manzanas / personas;
+  }
+}
