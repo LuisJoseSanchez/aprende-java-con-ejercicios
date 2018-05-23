9f03a34e1e8e06495b5770472a05eb20af84dd05
Luis José Sánchez <luisjose@espiral.io>
2018-05-23 16:43:09 +0200

    Ejercicio de la clase Movil.

--- soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio09/PruebaMoviles.java
+++ soluciones/09_POO/9.7.2_POO_en_Java/S972Ejercicio09/PruebaMoviles.java
@@ -1,21 +1,6 @@
-package ejercicio2;
-
-public class PruebaTerminales {
+public class PruebaMoviles {
 
   public static void main(String[] args) {
-//    Terminal t1 = new Terminal("678 11 22 33");
-//    Terminal t2 = new Terminal("644 74 44 69");
-//    Terminal t3 = new Terminal("622 32 89 09");
-//    Terminal t4 = new Terminal("664 73 98 18");
-//    System.out.println(t1);
-//    System.out.println(t2);
-//    t1.llama(t2, 320);
-//    t1.llama(t3, 200);
-//    System.out.println(t1);
-//    System.out.println(t2);
-//    System.out.println(t3);
-//    System.out.println(t4);
-
     Movil m1 = new Movil("678 11 22 33", "rata");
     Movil m2 = new Movil("644 74 44 69", "mono");
     Movil m3 = new Movil("622 32 89 09", "bisonte");
