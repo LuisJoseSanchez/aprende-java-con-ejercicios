369bffc30adb8db396db21dfe6c1d9c185a1ece5
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-05-24 18:42:22 +0200

    Arreglando tabulaciones.

--- soluciones/04_Sentencia_condicional/S04Ejercicio15.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio15.java
@@ -12,37 +12,37 @@
  */
 public class S04Ejercicio15 {
 
-	public static void main(String[] args) {
-				
-		System.out.println("Este programa pinta una pirámide.");
-		System.out.print("Introduzca un carácter de relleno: ");
-		String r = System.console().readLine();
-		System.out.println("Elija un tipo de pirámide");
-		System.out.println("1. Con el vértice hacia arriba");
-		System.out.println("2. Con el vértice hacia abajo");
-		System.out.println("3. Con el vértice hacia la izquierda");
-		System.out.println("4. Con el vértice hacia la derecha");
-		int opcion = Integer.parseInt(System.console().readLine());
+  public static void main(String[] args) {
+        
+    System.out.println("Este programa pinta una pirámide.");
+    System.out.print("Introduzca un carácter de relleno: ");
+    String r = System.console().readLine();
+    System.out.println("Elija un tipo de pirámide");
+    System.out.println("1. Con el vértice hacia arriba");
+    System.out.println("2. Con el vértice hacia abajo");
+    System.out.println("3. Con el vértice hacia la izquierda");
+    System.out.println("4. Con el vértice hacia la derecha");
+    int opcion = Integer.parseInt(System.console().readLine());
 
-		switch(opcion) {
-			case 1:
+    switch(opcion) {
+      case 1:
         System.out.println("  " + r);
         System.out.println(" " + r + r + r);
         System.out.println(r + r + r + r + r);
         break;
-			case 2:
+      case 2:
         System.out.println(r + r + r + r + r);
         System.out.println(" " + r + r + r);
         System.out.println("  " + r);
         break;
-			case 3:
+      case 3:
         System.out.println("    " + r);
         System.out.println("  " + r + " " + r);
         System.out.println(r + " " + r + " " + r);
         System.out.println("  " + r + " " + r);
         System.out.println("    " + r);
         break;
-			case 4:
+      case 4:
         System.out.println(r);
         System.out.println(r + " " + r);
         System.out.println(r + " " + r + " " + r);
@@ -50,6 +50,6 @@ public class S04Ejercicio15 {
         System.out.println(r);
         break;
       default:
-		}
-	}
+    }
+  }
 }
