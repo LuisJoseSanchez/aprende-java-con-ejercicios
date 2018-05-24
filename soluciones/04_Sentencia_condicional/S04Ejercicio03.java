326e853d147814041c67d72f24340b49413d3b86
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-05-24 18:39:39 +0200

    Arreglando tabulaciones.

--- soluciones/04_Sentencia_condicional/S04Ejercicio03.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio03.java
@@ -8,40 +8,40 @@
  */
 
 public class S04Ejercicio03 {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		String dia;
+    String dia;
 
-		System.out.print("Por favor, introduzca un número del 1 al 7: ");
-		int n = Integer.parseInt(System.console().readLine());
-		
-		switch(n) {
-			case 1:
+    System.out.print("Por favor, introduzca un número del 1 al 7: ");
+    int n = Integer.parseInt(System.console().readLine());
+    
+    switch(n) {
+      case 1:
         dia = "lunes";
         break;
-			case 2:
+      case 2:
         dia = "martes";
         break;
-			case 3:
+      case 3:
         dia = "miércoles";
         break;
-			case 4:
+      case 4:
         dia = "jueves";
         break;
-			case 5:
+      case 5:
         dia = "viernes";
         break;
-			case 6:
+      case 6:
         dia = "sábado";
         break;
-			case 7:
+      case 7:
         dia = "domingo";
         break;
-			default:
+      default:
         dia = "Debe introducir un número del 1 al 7";
-		}
-		
-		System.out.println(dia);
+    }
+    
+    System.out.println(dia);
 
-	}
+  }
 }
