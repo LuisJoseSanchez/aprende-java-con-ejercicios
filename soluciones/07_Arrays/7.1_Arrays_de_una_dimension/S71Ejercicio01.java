0340a7b9bf40b13e3c2ffd169951a9c6c027f057
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-05-26 20:21:34 +0200

    Arreglando formato.

--- soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio01.java
+++ soluciones/07_Arrays/7.1_Arrays_de_una_dimension/S71Ejercicio01.java
@@ -12,21 +12,21 @@
  */
 public class S71Ejercicio01 {
 
-	public static void main(String[] args) {
-		
-			int[] num = new int[12];
-			
-			num[0] = 39;
-			num[1] = -2;
-			num[4] = 0;
-			num[6] = 14;
-			num[8] = 5;
-			num[9] = 120;
-						
-			for (int i = 0; i < 12; i++) {
-				System.out.printf("num[%2d]: %d\n", i, num[i]);
+  public static void main(String[] args) {
+    
+      int[] num = new int[12];
+      
+      num[0] = 39;
+      num[1] = -2;
+      num[4] = 0;
+      num[6] = 14;
+      num[8] = 5;
+      num[9] = 120;
+            
+      for (int i = 0; i < 12; i++) {
+        System.out.printf("num[%2d]: %d\n", i, num[i]);
       }
       
   // Se puede observar que los valores del array se inicializan automáticamente a 0.
-	}
+  }
 }
