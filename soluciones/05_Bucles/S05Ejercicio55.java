49d8fe94fd7f39292cd2129ee170c2dc570390ab
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-06 20:43:57 +0200

    Añadiendo ejercicios de bucles.

---
+++ soluciones/05_Bucles/S05Ejercicio55.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class S05Ejercicio55 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.print("Introduzca un número: ");
+    long numeroIntroducido = Long.parseLong(s.nextLine());
+
+    long copia = numeroIntroducido;
+    int longitud = 0;
+
+    while (copia > 0) {
+      copia /= 10;
+      longitud++;
+    }
+
+    int ultimo = (int) (numeroIntroducido % 10);
+    long aux = numeroIntroducido / 10;
+    long resultado = ultimo * (long) Math.pow(10, longitud - 1) + aux;
+    System.out.println("El número resultado es " + resultado);
+  }
+
+}
