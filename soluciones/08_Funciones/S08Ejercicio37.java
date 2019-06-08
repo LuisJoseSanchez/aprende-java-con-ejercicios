39d71af564b136a2118d3529a94931f4c1969f68
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-09 00:19:49 +0200

    Actualizando ejercicios de funciones.

---
+++ soluciones/08_Funciones/S08Ejercicio37.java
@@ -0,0 +1,29 @@
+import static matematicas.Varias.digitoN;
+import static matematicas.Varias.digitos;
+
+public class S08Ejercicio37 {
+
+  public static void main(String[] args) {
+    int[] numeros = {213, 0, 11, 24, 86};
+    
+    for (int numero : numeros) {
+      System.out.print("El " + numero + " es el " + convierteEnMorse(numero));
+      System.out.println(" en morse.");
+    }
+  }
+  
+  public static String convierteEnMorse(int n) {
+    String[] morse = {
+	  "_ _ _ _ _", ". _ _ _ _", ". . _ _ _", ". . . _ _", ". . . . _",
+      ". . . . .", "_ . . . .", "_ _ . . .", "_ _ _ . .", "_ _ _ _ ."
+    };
+    
+    String resultado = "";
+    
+    for(int i = 0; i < digitos(n); i++) {
+      resultado += morse[digitoN(n, i)];
+    }
+    
+    return resultado;
+  }
+}
