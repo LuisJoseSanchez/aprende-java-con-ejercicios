cef0a4ce23d59938e0b3ad070a1151de461a23db
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-10 10:20:44 +0200

    Actualizando soluciones de ejercicios de funciones.

---
+++ soluciones/08_Funciones/S08Ejercicio39.java
@@ -0,0 +1,32 @@
+import static matematicas.Varias.digitoN;
+import static matematicas.Varias.digitos;
+
+public class S08Ejercicio39 {
+
+  public static void main(String[] args) {
+    int[] numeros = {470213, 2130, 9, 1124, 86};
+
+    for (int numero : numeros) {
+      System.out.println(numero);
+      System.out.println(convierteEnPalabras(numero) + "\n");
+    }
+  }
+
+  public static String convierteEnPalabras(int n) {
+    String[] digitos = {
+      "cero", "uno", "dos", "tres", "cuatro",
+      "cinco", "seis", "siete", "ocho", "nueve"
+    };
+
+    String resultado = "";
+
+    int i;
+    for (i = 0; i < digitos(n) - 1; i++) {
+      resultado += digitos[digitoN(n, i)] + ", ";
+    }
+    resultado += digitos[digitoN(n, i)];
+
+    return resultado;
+  }
+
+}
