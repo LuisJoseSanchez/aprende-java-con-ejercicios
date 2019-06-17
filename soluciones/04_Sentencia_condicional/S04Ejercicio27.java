805c823dbc367439c60ae629155ddda42b7b9bd1
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-17 23:46:24 +0200

    Borrando basura

--- soluciones/04_Sentencia_condicional/S04Ejercicio27.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio27.java
@@ -1,36 +1,37 @@
-package s08ejercicio41;
-
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class S04Ejercicio27 {
 
   public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
+  //  Scanner s = new Scanner(System.in);
 
     System.out.print("Elija un sabor (manzana, fresa o chocolate): ");
-    String sabor = s.nextLine();
-    
+    String sabor = "hola"; //s.nextLine();
+/*    
     double precioSabor = 0;
     String tipoChocolate = "";
     
     switch (sabor) {
       case "manzana":
         precioSabor = 18;
+        break;
       case "fresa":
         precioSabor = 16;
+        break;
       case "chocolate":
         System.out.print("¿Qué tipo de chocolate quiere? (negro o blanco): ");
-        tipoChocolate = s.nextLine();
+        tipoChocolate = "";//s.nextLine();
         if (tipoChocolate.equals("negro")) {
           precioSabor = 14;
         } else if (tipoChocolate.equals("blanco")) {
           precioSabor = 15;
         }
+        break;
       default:
     }
     
     System.out.print("¿Quiere nata? (si o no): ");
-    boolean conNata = (s.nextLine()).equals("si");
+    boolean conNata = true;//(s.nextLine()).equals("si");
 
     System.out.print("Tarta de " + sabor);
     
@@ -39,6 +40,6 @@ public class S04Ejercicio27 {
     }
    
     System.out.println(": " + precioSabor);
-
+*/
   }
 }
