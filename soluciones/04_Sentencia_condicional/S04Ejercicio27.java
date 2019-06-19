5ff3407f9b50a41db636603c698fb07509c3fcba
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-19 18:32:43 +0200

    Actualizando ejercicios.

--- soluciones/04_Sentencia_condicional/S04Ejercicio27.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio27.java
@@ -1,16 +1,20 @@
-//import java.util.Scanner;
+package s04ejercicio27;
+
+import java.util.Scanner;
 
 public class S04Ejercicio27 {
 
   public static void main(String[] args) {
-  //  Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
 
     System.out.print("Elija un sabor (manzana, fresa o chocolate): ");
-    String sabor = "hola"; //s.nextLine();
-/*    
+    String sabor = s.nextLine();
+
     double precioSabor = 0;
+    double precioNata = 0;
+    double precioNombre = 0;
     String tipoChocolate = "";
-    
+
     switch (sabor) {
       case "manzana":
         precioSabor = 18;
@@ -20,7 +24,7 @@ public class S04Ejercicio27 {
         break;
       case "chocolate":
         System.out.print("¿Qué tipo de chocolate quiere? (negro o blanco): ");
-        tipoChocolate = "";//s.nextLine();
+        tipoChocolate = s.nextLine();
         if (tipoChocolate.equals("negro")) {
           precioSabor = 14;
         } else if (tipoChocolate.equals("blanco")) {
@@ -29,17 +33,32 @@ public class S04Ejercicio27 {
         break;
       default:
     }
-    
+
     System.out.print("¿Quiere nata? (si o no): ");
-    boolean conNata = true;//(s.nextLine()).equals("si");
+    boolean conNata = s.nextLine().equals("si");
+    
+    System.out.print("¿Quiere ponerle un nombre? (si o no): ");
+    boolean conNombre = s.nextLine().equals("si");
 
     System.out.print("Tarta de " + sabor);
-    
+
     if (sabor.equals("chocolate")) {
-      System.out.print(tipoChocolate);
+      System.out.print(" " + tipoChocolate);
     }
-   
-    System.out.println(": " + precioSabor);
-*/
+
+    System.out.printf(": %.2f €\n", precioSabor);
+    
+    if (conNata) {
+      precioNata = 2.5;
+      System.out.printf("Con nata: %.2f €\n", precioNata);
+    }
+    
+    if (conNombre) {
+      precioNombre = 2.75;
+      System.out.printf("Con nombre: %.2f €\n", precioNombre);
+    }
+    
+    System.out.printf("Total: %.2f €\n", precioSabor + precioNata + precioNombre);
   }
+
 }
