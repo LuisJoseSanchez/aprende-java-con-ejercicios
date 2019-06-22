7beef766aba49093221ab9f57129c91e7a3c415b
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-22 23:19:51 +0200

    Añadiendo ejercicios de funciones.

---
+++ soluciones/08_Funciones/S08Ejercicio47.java
@@ -0,0 +1,24 @@
+public class S08Ejercicio47 {
+
+  public static void main(String[] args) {
+
+    int[] a = {};
+    System.out.println("a: " + convierteArrayEnString(a));
+
+    int[] b = {8};
+    System.out.println("b: " + convierteArrayEnString(b));
+    
+    int[] c = {6, 2, 5, 0, 1};
+    System.out.println("b: " + convierteArrayEnString(c));
+
+  }
+
+  public static String convierteArrayEnString(int[] a) {
+    String resultado = "";
+    for (int digito : a) {
+      resultado += digito;
+    }
+    return resultado;
+  }
+
+}
