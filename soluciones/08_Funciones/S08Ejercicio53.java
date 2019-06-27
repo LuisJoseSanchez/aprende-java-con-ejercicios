475e1744b06a2f74f2dd255e74862033bbf1e35e
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-27 12:03:52 +0200

    Añadiendo ejercicios de funciones.

---
+++ soluciones/08_Funciones/S08Ejercicio53.java
@@ -0,0 +1,32 @@
+public class S08Ejercicio53 {
+
+  public static void main(String[] args) {
+    int[][] array = new int[4][6];
+    int fila;
+    int columna;
+
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 6; columna++) {
+        array[fila][columna] = (int) (Math.random() * 201);
+        System.out.printf("%5d ", array[fila][columna]);
+      }
+      System.out.println("");
+    }
+    System.out.println(nEsimo(array, 3));
+    System.out.println(nEsimo(array, 20));
+    System.out.println(nEsimo(array, 24));
+    System.out.println(nEsimo(array, 23));
+  }
+
+  public static int nEsimo(int[][] n, int posicion) {
+	int filas = n.length;
+    int columnas = n[0].length;
+    
+    if ((posicion < 0) || (posicion > filas * columnas - 1)) {
+      return -1;
+    } else {
+	  return n[posicion / columnas][posicion % columnas];
+	}
+  }
+  
+}
