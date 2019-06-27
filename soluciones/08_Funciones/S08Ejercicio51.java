a0cd93e1012ca779ba0b236ea012c1e349e43336
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-27 11:45:45 +0200

    Añadiendo ejercicios de funciones.

---
+++ soluciones/08_Funciones/S08Ejercicio51.java
@@ -0,0 +1,29 @@
+import array.Array;
+import matematicas.Varias;
+
+public class S08Ejercicio51 {
+
+  public static void main(String[] args) {
+    int[] a = Array.generaArrayInt(10, 2, 100);
+    
+    System.out.println("Array generado:");
+    Array.muestraArrayInt(a);
+    
+    System.out.println();
+    
+    for (int n : a) {
+      if (Varias.esPrimo(n)) {
+        System.out.print("El " + n + " es primo");
+      } else {
+        System.out.print("El " + n + " no es primo");
+      }
+      
+      if (Varias.esCapicua(n)) {
+        System.out.println(" y es capicúa.");
+      } else {
+        System.out.println(" y no es capicúa.");
+      }
+    }
+  }
+  
+}
