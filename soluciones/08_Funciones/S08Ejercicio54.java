48aa8f985bb44d33bb7e173e6098ae7734aafae9
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-29 16:22:44 +0200

    Añadiendo ejercicios de números funciones.

---
+++ soluciones/08_Funciones/S08Ejercicio54.java
@@ -0,0 +1,40 @@
+public class S08Ejercicio54 {
+
+  public static void main(String[] args) {
+    System.out.println(ocurrencias(8, 4672));
+    System.out.println(ocurrencias(5, 5251535));
+    System.out.println(ocurrencias(2, 123456));
+
+    int[] a = {714, 81, 9, 11};
+    System.out.println(ocurrencias(1, a));
+    
+    int[] b = {42, 13, 12345, 4};
+    System.out.println(ocurrencias(4, b));
+    
+    int[] c = {6, 66, 666};
+    System.out.println(ocurrencias(6, c));
+  }
+  
+  public static int ocurrencias(int digito, int n) {
+    int repetido = 0;
+    
+    while (n > 0) {
+      if ((n % 10) == digito) {
+        repetido++;
+      }
+      n = n/10;
+    }
+
+    return repetido;
+  }
+  
+  public static int ocurrencias(int digito, int[] a) {
+    int repetido = 0;
+    
+    for (int n : a) {
+      repetido += ocurrencias(digito, n);
+    }
+    
+    return repetido;
+  }
+}
