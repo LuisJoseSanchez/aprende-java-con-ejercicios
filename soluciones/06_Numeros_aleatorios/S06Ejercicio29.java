5b6be7bd879e4f6b13e8c01b0feea5f262346e12
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-29 23:12:51 +0200

    Añadiendo ejercicios de números aleatorios.

---
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio29.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class S06Ejercicio29 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println("1. Primavera");
+    System.out.println("2. Verano");
+    System.out.println("3. Otoño");
+    System.out.println("4. Invierno");
+    System.out.print("Seleccione la estación del año (1-4): ");
+    int estacion = Integer.parseInt(s.nextLine());
+
+    int temperaturaMinima = 0;
+    int temperaturaMaxima = 0;
+    String cielo = "";
+    
+    switch(estacion) {
+      case 1: // Primavera
+        temperaturaMinima = (int)(Math.random() * 16 + 15); 
+        temperaturaMaxima = (int)(Math.random() * 16 + 15);
+        cielo = Math.random() <= 0.6 ? "Soleado" : "Nublado";
+        break;
+      case 2: // Verano
+        temperaturaMinima = (int)(Math.random() * 21 + 25); 
+        temperaturaMaxima = (int)(Math.random() * 21 + 25);
+        cielo = Math.random() <= 0.8 ? "Soleado" : "Nublado";
+        break;
+      case 3: // Otoño
+        temperaturaMinima = (int)(Math.random() * 11 + 20); 
+        temperaturaMaxima = (int)(Math.random() * 11 + 20);
+        cielo = Math.random() <= 0.4 ? "Soleado" : "Nublado";
+        break;
+      case 4: // Invierno
+        temperaturaMinima = (int)(Math.random() * 26); 
+        temperaturaMaxima = (int)(Math.random() * 26);
+        cielo = Math.random() <= 0.2 ? "Soleado" : "Nublado";
+        break;
+      default:
+        System.out.println("La estación seleccionada no es correcta.");
+    }
+    
+    // Si la mínima es mayor, se intercambian las temperaturas
+    if (temperaturaMinima > temperaturaMaxima) {
+      int aux = temperaturaMinima;
+      temperaturaMinima = temperaturaMaxima;
+      temperaturaMaxima = aux;
+    }
+    
+    System.out.println("Previsión del tiempo para mañana");
+    System.out.println("--------------------------------");
+    System.out.println("Temperatura mínima: " + temperaturaMinima);
+    System.out.println("Temperatura máxima: " + temperaturaMaxima);
+    System.out.println(cielo);
+  }
+  
+}
