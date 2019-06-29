b44510df21c8ad93bf29e648f482bca476d3223e
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-06-29 15:33:29 +0200

    Actualizando ejercicios de POO.

--- soluciones/09_POO/9.2_POO_en_Java/S92Ejercicio12/Biblioteca.java
+++ soluciones/09_POO/9.2_POO_en_Java/S92Ejercicio12/Biblioteca.java
@@ -3,7 +3,7 @@ public class Biblioteca {
   public static void main(String[] args) {
     Libro libro1 = new Libro("123456", "La Ruta Prohibida", 2007);
     Libro libro2 = new Libro("112233", "Los Otros", 2016);
-    Libro libro3 = new Libro("456789", "La rosa del mundo Tapa blanda", 1995);
+    Libro libro3 = new Libro("456789", "La rosa del mundo", 1995);
     Revista revista1 = new Revista("444555", "Año Cero", 2019, 344);
     Revista revista2 = new Revista("002244", "National Geographic", 2003, 255);
     System.out.println(libro1);
