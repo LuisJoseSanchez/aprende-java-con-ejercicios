c30da7361a0f4922a1dd03c9e3028ec7295a580c
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-07-02 16:54:32 +0200

    Añadiendo ejercicios de números aleatorios (juego del Craps).

---
+++ soluciones/06_Numeros_aleatorios/S06Ejercicio31.java
@@ -0,0 +1,11 @@
+package s06ejercicio31;
+
+public class S06Ejercicio31 {
+
+  public static void main(String[] args) {
+    System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+    System.out.println("┃ ⚀ C ⚁ R ⚂ A ⚃ P ⚄ S ⚅ ┃");
+    System.out.println("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
+  }
+  
+}
