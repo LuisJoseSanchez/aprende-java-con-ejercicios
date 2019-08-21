3bd12056579f3d426d485671dc04c1e8be837a42
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-08-21 17:34:52 +0200

    Actualizando ejemplos del capítulo 1.

--- soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio10.java
+++ soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio10.java
@@ -1,27 +1,23 @@
 /**
- * 
  * 1. Salida por pantalla
  *
- * 9. Escribe un programa que pinte por pantalla alguna escena - el campo,
- *    la habitación de una casa, un aula, etc. - o algún objeto animado o
- *    inanimado - un coche, un gato, una taza de café, etc. Ten en cuenta
- *    que puedes utilizar caracteres como *, +, <, #, @, etc. o incluso
- *    caracteres Unicode. ¡Échale imaginación!
+ * 10. Mejora el ejercicio anterior añadiéndole colores.
  *
  * @author Luis José Sánchez
- * 
  */
-public class S01Ejercicio09 {
+public class S01Ejercicio10 {
   public static void main(String[] args) {
-    System.out.println("\n                        ██");
+    System.out.println("\033[31m");
+    System.out.println("                        ██");
     System.out.println("    APRENDE JAVA      ██");
-    System.out.println("   CON EJERCICIOS       ██\n");
-    System.out.println("  Luis José Sánchez  ████████████");
+    System.out.println("   CON EJERCICIOS       ██\n\033[33m");
+    System.out.println("  Luis José Sánchez \033[34m ████████████");
     System.out.println("                     ████████  ██");
     System.out.println("                     ████████████");
     System.out.println("                     ████████");
     System.out.println("░░░░░░░░░░░░░░░░░░░░░████████░░░░░░░░");
     System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
     System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("\033[37m"); // volvemos al blanco
   }
 }
