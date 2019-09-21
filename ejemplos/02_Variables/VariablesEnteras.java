0d2f7f9e712fafb09228f878a36d4d19ec3c481e
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-09-21 20:31:01 +0200

    Actualizando ejemplos del capítulo 2.

--- ejemplos/02_Variables/VariablesEnteras.java
+++ ejemplos/02_Variables/VariablesEnteras.java
@@ -3,15 +3,18 @@
  *
  * @author Luis J. Sánchez
  */
- 
 public class VariablesEnteras {
   public static void main(String[] args) {
-    int x; // Se declara la variable x como entera
+    int x; // Declara la variable x como entera
 
-    x = 5; // Se asigna el valor 5 a la variable x
+    x = 5; // Asigna el valor 5 a la variable x
+
+	// Muestra el valor de x
     System.out.println("El valor actual de mi variable es " + x);
 
-    x = 7; // Se asigna el valor 7 a la variable x
+    x = 7; // Asigna el valor 7 a la variable x
+    
+    // Muestra de nuevo el valor de x
     System.out.println("y ahora es " + x);
   }
 }
