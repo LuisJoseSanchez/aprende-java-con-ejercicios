ae24200897051545ede8d26cb491357c135869de
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-09-22 10:50:23 +0200

    Actualizando ejemplos del capítulo 2.

--- ejemplos/02_Variables/ConversionDeTipos.java
+++ ejemplos/02_Variables/ConversionDeTipos.java
@@ -3,17 +3,16 @@
  *
  * @author Luis J. Sánchez
  */
-
 public class ConversionDeTipos {
   public static void main(String[] args) {
-
     int x = 2;
     int y = 9;
     double division;
 
-    division = (double)y / (double)x;
-    //division = y / x; // Comenta esta línea y
-                // observa la diferencia.
+    division = (double) y / (double) x;
+    
+    // Descomenta la siguiente línea y observa cómo cambia el resultado.
+    // division = y / x;
   
     System.out.println("El resultado de la división es " + division);   
   }
