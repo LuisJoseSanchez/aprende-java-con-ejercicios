2c4623b74de0d7be6c788eb29e5049b2b68035fe
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-09-22 10:21:09 +0200

    Actualizando ejemplos del capítulo 2.

---
+++ ejemplos/02_Variables/UsoDeChar.java
@@ -0,0 +1,17 @@
+/**
+ * Uso del tipo char
+ *
+ * @author Luis J. Sánchez
+ */
+public class UsoDeChar {
+  public static void main(String[] args) {
+    char letra1 = 'c';
+    char letra2 = 'a';
+    char letra3 = 's';
+    char letra4 = 'a';
+    
+    System.out.println("letra1: " + letra1);
+    System.out.println("letra3: " + letra3);
+    System.out.println("todas las letras juntas: " + letra1 + letra2 + letra3 + letra4);
+  }
+}
