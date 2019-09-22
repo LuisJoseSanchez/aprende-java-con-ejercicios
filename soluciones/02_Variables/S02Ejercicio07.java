393f0c85e01e2f83f0991d8ffa975d453de50a10
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-09-22 11:23:24 +0200

    Actualizando ejercicios del capítulo 2.

--- soluciones/02_Variables/S02Ejercicio07.java
+++ soluciones/02_Variables/S02Ejercicio07.java
@@ -4,6 +4,6 @@ public class S02Ejercicio07 {
     char ultimaLetra = 'z';
     String palabra = "abecedario";
     
-    System.out.println(primeraLetra + ultimaLetra + palabra);
+    System.out.println(primeraLetra + "" + ultimaLetra + palabra);
   }
 }
