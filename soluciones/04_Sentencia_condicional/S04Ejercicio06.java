9fca20e693b8f9cc688ea07bdd316307d3a799ef
Luis José Sánchez <luisjoseprofe@gmail.com>
2020-10-28 08:43:19 +0100

    IMPORTANTE: Corregida la declaración de la constante g.

--- soluciones/04_Sentencia_condicional/S04Ejercicio06.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio06.java
@@ -8,16 +8,13 @@
  * @author Luis José Sánchez
  */
 public class S04Ejercicio06 {
-
-  // las constantes se declaran fuera del main
-  final static double g = 9.81;
-
   public static void main(String[] args) {
 
     System.out.println("Cálculo del tiempo de caída de un objeto.");
     System.out.print("Por favor, introduzca la altura (en metros) desde la que cae el objeto: ");
     Double h = Double.parseDouble(System.console().readLine());
 
+    final double g = 9.81; // las constantes se declaran con final
     double s = Math.sqrt(2*h/g);
 
     System.out.printf("El objeto tarda %.2f segundos en caer.\n", s);
