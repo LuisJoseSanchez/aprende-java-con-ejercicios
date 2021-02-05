94adc0216f3ed8b7e5980a0b5361aa1fdef94b8d
Luis José Sánchez <luisjoseprofe@gmail.com>
2021-02-05 16:44:31 +0100

    Update S04Ejercicio04.java
    En la primera parte del if se deben contar las primeras 40 horas, no las primeras 39.

--- soluciones/04_Sentencia_condicional/S04Ejercicio04.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio04.java
@@ -17,7 +17,7 @@ public class S04Ejercicio04 {
     System.out.print("Por favor, introduzca el número de horas trabajadas durante la semana: ");
     int horasTrabajadas = Integer.parseInt(System.console().readLine());
     
-    if (horasTrabajadas < 40) {
+    if (horasTrabajadas <= 40) {
       sueldoSemanal = 12 * horasTrabajadas;
     } else {
       sueldoSemanal = (40 * 12) + ((horasTrabajadas - 40) * 16);
