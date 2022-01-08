38c663b2fdc8b7dc3824ae07f48c5e0c38cfdb11
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-01-08 18:28:23 +0100

    Actualizando las soluciones de los ejercicios

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio02.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio02.java
@@ -8,11 +8,11 @@
  */
 public class S03Ejercicio02 {
   public static void main(String[] args) {
-    System.out.print("Por favor, introduce la cantidad de euros que quieres convertir: ");
+    System.out.print("Por favor, introduzca la cantidad de euros que quiere convertir: ");
     double euros = Double.parseDouble(System.console().readLine());
 
     int pesetas = (int) (euros * 166.386);
     
-    System.out.print(euros + " euros son " + pesetas + " pesetas.");
+    System.out.printf("%.2f euros son %d pesetas.", euros, pesetas);
   }
 }
