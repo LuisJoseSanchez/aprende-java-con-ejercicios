31666bd85466f7fde1c4840ab3b42543a00b64ff
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-04-18 19:38:56 +0200

    Actualizando ejemplos del capítulo 6

--- ejemplos/06_Numeros_aleatorios/Aleatorio07.java
+++ ejemplos/06_Numeros_aleatorios/Aleatorio07.java
@@ -7,31 +7,31 @@ public class Aleatorio07 {
   public static void main(String[] args) {
     System.out.println("Muestra un día de la semana al azar:");
 
-      int  dia = (int)(Math.random() * 7) + 1; // genera un número aleatorio
-                                               // entre el 1 y el 7
-      switch(dia) {
-        case 1:
-          System.out.println("lunes");
-          break;
-        case 2:
-          System.out.println("martes");
-          break;
-        case 3:
-          System.out.println("miércoles");
-          break;
-        case 4:
-          System.out.println("jueves");
-          break;
-        case 5:
-          System.out.println("viernes");
-          break;
-        case 6:
-          System.out.println("sábado");
-          break;
-        case 7:
-          System.out.println("domingo");
-          break;
-        default:
-      }
+    int  dia = (int)(Math.random() * 7) + 1; // genera un número aleatorio
+                                             // entre el 1 y el 7
+    switch(dia) {
+      case 1:
+        System.out.println("lunes");
+        break;
+      case 2:
+        System.out.println("martes");
+        break;
+      case 3:
+        System.out.println("miércoles");
+        break;
+      case 4:
+        System.out.println("jueves");
+        break;
+      case 5:
+        System.out.println("viernes");
+        break;
+      case 6:
+        System.out.println("sábado");
+        break;
+      case 7:
+        System.out.println("domingo");
+        break;
+      default:
+    }
   }
 }
