6ea27346544f944e72f7416aba1d350c09d81825
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-04-18 19:20:43 +0200

    Actualizando ejemplos del capítulo 6

--- ejemplos/06_Numeros_aleatorios/Aleatorio02.java
+++ ejemplos/06_Numeros_aleatorios/Aleatorio02.java
@@ -5,11 +5,11 @@
  */
 public class Aleatorio02 {
   public static void main(String[] args) {
-    System.out.println("20 números aleatorios entre 0 y 10");
+    System.out.print("20 números aleatorios entre 0 y 10");
     System.out.println(" sin llegar a 10 (con decimales):");
     
     for (int i = 1; i <= 20; i++) {
-      System.out.println( Math.random()*10 + "  ");
+      System.out.println(Math.random() * 10 + "  ");
     }
   }
 }
