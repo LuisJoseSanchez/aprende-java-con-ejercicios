4e56cff54f17fc0945e10b8cbbd63afd722b0b6d
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-05-18 12:49:39 +0200

    Actualizando soluciones del capítulo 9

--- soluciones/09_POO/9.2_POO_en_Java/S92Ejercicio07/ExpocochesMalaga.java
+++ soluciones/09_POO/9.2_POO_en_Java/S92Ejercicio07/ExpocochesMalaga.java
@@ -22,7 +22,7 @@
  *    </code>
  * @author Luis José Sánchez
  */
-public class ExpocochesCampanillas {
+public class ExpocochesMalaga {
   public static void main(String[] args) {
     
     Zona principal = new Zona(1000);
@@ -34,22 +34,22 @@ public class ExpocochesCampanillas {
     int n = 0;
     
     do {
-      System.out.println("\n\nEXPOCOCHES CAMPANILLAS");
+      System.out.println("\nEXPOCOCHES MÁLAGA");
       System.out.println("1. Mostrar número de entradas libres");
       System.out.println("2. Vender entradas");
       System.out.println("3. Salir");
-      System.out.println("Elige una opción: ");
+      System.out.print("Elige una opción: ");
       
       opcion = Integer.parseInt(System.console().readLine());
       
       if (opcion == 1) {
-        System.out.println("\n\nEn la zona principal hay " + principal.getEntradasPorVender());
+        System.out.println("\nEn la zona principal hay " + principal.getEntradasPorVender());
         System.out.println("En la zona de compra venta hay " + compraVenta.getEntradasPorVender());
         System.out.println("En la zona vip hay " + vip.getEntradasPorVender());
       }
       
       if (opcion == 2) {
-        System.out.println("\n\n1. Principal");
+        System.out.println("\n1. Principal");
         System.out.println("2. Compra-venta");
         System.out.println("3. Vip");
         System.out.print("Elige la zona para la que quieres comprar las entradas: ");
